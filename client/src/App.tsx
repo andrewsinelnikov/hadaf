4cce6eaa6fb34842294f00cf4aaf954786dfa40d
Andrew <ansinelnikov@gmail.com>
2022-03-31 18:40:42 +0300

    styled container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import PageRender from "./PageRender";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hadaf</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<PageRender />} />
+            <Route path=':page' element={<PageRender />} />
+            <Route path=':page/:slug' element={<PageRender />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 };
 
