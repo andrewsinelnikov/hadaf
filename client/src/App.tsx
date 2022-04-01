b497951619626f9cc2b20ccff7ea99ecb2cd6593
Andrew <ansinelnikov@gmail.com>
2022-04-01 10:02:42 +0300

    added Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PageRender from "./PageRender";
+import Navbar from "./components/global/Navbar";
 
 const App = () => {
   return (
     <>
       <Router>
+        <Navbar />
         <div className='container'>
           <Routes>
             <Route path='/' element={<PageRender />} />
