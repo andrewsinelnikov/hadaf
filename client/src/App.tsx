bb1e84b3c3ab563eda644c7e283e2943829ed508
Andrew <ansinelnikov@gmail.com>
2022-04-02 10:19:31 +0300

    changed structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PageRender from "./PageRender";
-import Navbar from "./components/global/Navbar";
 
 const App = () => {
   return (
     <>
       <Router>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<PageRender />} />
-            <Route path=':page' element={<PageRender />} />
-            <Route path=':page/:slug' element={<PageRender />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='/' element={<PageRender />} />
+          <Route path=':page' element={<PageRender />} />
+          <Route path=':page/:slug' element={<PageRender />} />
+        </Routes>
       </Router>
     </>
   );
--- client/src/components/layouts/MainPageLayout.tsx
+++ client/src/components/layouts/MainPageLayout.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../global/Navbar";
 
 const MainPageLayout: React.FC<{}> = (props) => {
+  // https://stackoverflow.com/questions/65605219/rendre-footer-component-all-page-except-404-pages-in-react
+
   return (
-    <div>
+    <>
       <Navbar />
-      {props.children}
-    </div>
+      <div className='container'>{props.children}</div>
+    </>
   );
 };
 
