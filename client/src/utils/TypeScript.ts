6486adc87f86abc19fa0327bac51ddb6f12836d1
Andrew <ansinelnikov@gmail.com>
2022-04-03 18:28:49 +0300

    added InputChange type

--- client/src/utils/TypeScript.ts
+++ client/src/utils/TypeScript.ts
@@ -1,3 +1,9 @@
+import { ChangeEvent, FormEvent } from "react";
+
+export type InputChange = ChangeEvent<
+  HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+>;
+
 export interface IParams {
   page: string;
   slug: string;
