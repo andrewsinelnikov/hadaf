7eb00aa18d498640925e90caa222035fd0710567
Andrew <ansinelnikov@gmail.com>
2022-04-03 18:44:29 +0300

    added LoginPass structure

--- client/src/components/auth/LoginPass.tsx
+++ client/src/components/auth/LoginPass.tsx
@@ -5,6 +5,7 @@ const LoginPass = () => {
   const initialState = { account: "", password: "" };
   const [userLogin, setUserLogin] = useState(initialState);
   const { account, password } = userLogin;
+  const [typePass, setTypePass] = useState(false);
 
   const handleChangeInput = (e: InputChange) => {
     const { value, name } = e.target;
@@ -12,9 +13,47 @@ const LoginPass = () => {
   };
 
   return (
-    <div>
-      <h2>pass</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className='form-group mb-3'>
+        <label htmlFor='account' className='form-label'>
+          Email / Phone number
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          name='account'
+          id='account'
+          value={account}
+          onChange={handleChangeInput}
+        />
+      </div>
+
+      <div className='form-group mb-3'>
+        <label htmlFor='password' className='form-label'>
+          Password
+        </label>
+        <div className='pass'>
+          <input
+            type={typePass ? "text" : "password"}
+            className='form-control'
+            name='password'
+            id='password'
+            value={password}
+            onChange={handleChangeInput}
+          />
+          <small onClick={(e) => setTypePass(!typePass)}>
+            {typePass ? "Hide" : "Show"}
+          </small>
+        </div>
+      </div>
+
+      <button
+        type='submit'
+        className='btn btn-dark w-100 mt-1'
+        disabled={account && password ? false : true}>
+        Login
+      </button>
+    </form>
   );
 };
 
