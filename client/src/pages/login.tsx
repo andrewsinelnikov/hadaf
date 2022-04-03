b8cd202851733e2778f6f2cbade0bd855f30b44c
Andrew <ansinelnikov@gmail.com>
2022-04-03 18:22:50 +0300

    added LoginPass

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import LoginPass from "../components/auth/LoginPass";
+
 const Login = () => {
   return (
     <div className='auth_page'>
       <div className='auth_box'>
         <h3 className='text-uppercase text-center mb-4'>Login</h3>
+
+        <LoginPass />
       </div>
     </div>
   );
