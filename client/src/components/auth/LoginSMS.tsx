148d579c66dada2dd55a1728fe6f462c85081d90
Andrew <ansinelnikov@gmail.com>
2022-04-04 09:20:02 +0300

    added LoginSMS structure

--- client/src/components/auth/LoginSMS.tsx
+++ client/src/components/auth/LoginSMS.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { useState } from "react";
 
 const LoginSMS = () => {
+  const [phone, setPhone] = useState("");
+
   return (
-    <div>
-      <h2>LoginSMS</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className='form-group mb-3'>
+        <label htmlFor='phone' className='form-label'>
+          Phone number
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          name='phone'
+          id='phone'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder='+380321231111'
+        />
+      </div>
+
+      <button
+        type='submit'
+        className='btn btn-dark w-100 mt-1'
+        disabled={phone ? false : true}>
+        Login
+      </button>
+    </form>
   );
 };
 
