1743eb2865c81211ccae9002cbee450490a3ee52
Andrew <ansinelnikov@gmail.com>
2022-04-04 09:57:21 +0300

    added login action

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -1,3 +1,21 @@
 import { Dispatch } from "redux";
+import { AUTH, IAuthType } from "./../types/authType";
 
 import { IUserLogin } from "../../utils/TypeScript";
+import { postAPI } from "../../utils/FetchData";
+
+export const login =
+  (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType>) => {
+    try {
+      //   dispatch({ type: ALERT, payload: { loading: true } });
+      const res = await postAPI("login", userLogin);
+
+      dispatch({ type: AUTH, payload: res.data });
+
+      //   dispatch({ type: ALERT, payload: { success: res.data.msg } });
+      localStorage.setItem("logged", "true");
+    } catch (err: any) {
+      //   dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      console.log(err.response.data.msg);
+    }
+  };
