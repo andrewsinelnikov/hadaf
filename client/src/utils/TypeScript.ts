5cdab7a427539dbaccd8f6935fd6e4111282edf6
Andrew <ansinelnikov@gmail.com>
2022-04-04 09:42:21 +0300

    added IUser interface

--- client/src/utils/TypeScript.ts
+++ client/src/utils/TypeScript.ts
@@ -15,3 +15,13 @@ export interface IUserLogin {
   account: string;
   password: string;
 }
+
+export interface IUser extends IUserLogin {
+  avatar: string;
+  createdAt: string;
+  name: string;
+  role: string;
+  type: string;
+  updatedAt: string;
+  _id: string;
+}
