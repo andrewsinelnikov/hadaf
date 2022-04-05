1fb46325781852e9d95b983ef476c87bb6e530d3
Andrew <ansinelnikov@gmail.com>
2022-04-05 09:39:26 +0300

    added alert

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -1,21 +1,22 @@
 import { Dispatch } from "redux";
 import { AUTH, IAuthType } from "./../types/authType";
+import { ALERT, IAlertType } from "./../types/alertType";
 
 import { IUserLogin } from "../../utils/TypeScript";
 import { postAPI } from "../../utils/FetchData";
 
 export const login =
-  (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType>) => {
+  (userLogin: IUserLogin) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
     try {
-      //   dispatch({ type: ALERT, payload: { loading: true } });
+      dispatch({ type: ALERT, payload: { loading: true } });
       const res = await postAPI("login", userLogin);
 
       dispatch({ type: AUTH, payload: res.data });
 
-      //   dispatch({ type: ALERT, payload: { success: res.data.msg } });
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
       localStorage.setItem("logged", "true");
     } catch (err: any) {
-      //   dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-      console.log(err.response.data.msg);
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
     }
   };
