e8c51ef84fbf01625cd96b3a375ffa53483029e3
Andrew <ansinelnikov@gmail.com>
2022-04-05 10:25:48 +0300

    added Alert

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PageRender from "./PageRender";
+import Alert from "./components/alert/Alert";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
+          <Alert />
           <Route path='/' element={<PageRender />} />
           <Route path=':page' element={<PageRender />} />
           <Route path=':page/:slug' element={<PageRender />} />
