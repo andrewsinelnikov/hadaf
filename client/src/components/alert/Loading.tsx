0b60819d940db2d66887c7730e53a73bb1e5a078
Andrew <ansinelnikov@gmail.com>
2022-04-05 10:48:32 +0300

    added animation

--- client/src/components/alert/Loading.tsx
+++ client/src/components/alert/Loading.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const Loading = () => {
   return (
     <div className='loading'>
-      <h2>Loading</h2>
+      <svg width='205' height='250' viewBox='0 0 40 50'>
+        <i className='fa fa-long-arrow-right arrow' aria-hidden='true'></i>
+        <text fill='#e7e0b9' x='5' y='47'>
+          Hadaf
+        </text>
+      </svg>
     </div>
   );
 };
