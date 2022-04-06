7370ffb57a0c09c3f248d15cd776a457417edeea
Andrew <ansinelnikov@gmail.com>
2022-04-06 10:12:21 +0300

    added validate functions

--- client/src/utils/Validate.ts
+++ client/src/utils/Validate.ts
@@ -24,3 +24,22 @@ export const validateRegister = (userRegister: IUserRegister) => {
     errLength: errors.length,
   };
 };
+
+export const checkPassword = (password: string, cf_password: string) => {
+  if (password.length < 8) {
+    return "Password must be at least 8 chars long";
+  } else if (password !== cf_password) {
+    return "Passwords do not match";
+  }
+};
+
+export const validateEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+export const validatePhone = (phone: string) => {
+  const re = /^[+]/g;
+  return re.test(phone);
+};
