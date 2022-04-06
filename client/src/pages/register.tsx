892834f1daf281578b93b11635921bc2f9ae588e
Andrew <ansinelnikov@gmail.com>
2022-04-06 09:46:41 +0300

    changed Register structure

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import RegisterForm from "../components/auth/RegisterForm";
 
 const Register = () => {
+  const location = useLocation();
+
   return (
-    <div>
-      <h2>Register page</h2>
+    <div className='auth_page'>
+      <div className='auth_box'>
+        <h3 className='text-uppercase text-center mb-4'>Register</h3>
+        <RegisterForm />
+
+        <p className='mt-2'>
+          {`Already have an account? `}
+          <Link to={`/login${location.search}`} style={{ color: "crimson" }}>
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
