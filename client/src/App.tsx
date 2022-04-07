d9efacc662fe2094c2deec05486ca3ddb054b23f
Andrew <ansinelnikov@gmail.com>
2022-04-07 09:22:24 +0300

    changed structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PageRender from "./PageRender";
-import Alert from "./components/alert/Alert";
+import { Alert } from "./components/alert/Alert";
 
 const App = () => {
   return (
--- client/src/pages/active/[slug].tsx
+++ client/src/pages/active/[slug].tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { IParams } from "../../utils/TypeScript";
 
 const Active = () => {
+  const { slug }: IParams = useParams<keyof IParams>() as IParams;
+
   return (
     <div>
       <h2>Active</h2>
