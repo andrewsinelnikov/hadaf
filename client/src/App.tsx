f08fec41a6bfbbe9a4a973538ff014c363527114
Andrew <ansinelnikov@gmail.com>
2022-04-07 09:51:51 +0300

    added refreshToken

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { refreshToken } from "./redux/actions/authAction";
 
 import PageRender from "./PageRender";
 import { Alert } from "./components/alert/Alert";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
     <>
       <Router>
