ae3f4400514ea3e11d70f1a8b0c32af62074f626
Andrew <ansinelnikov@gmail.com>
2022-04-08 21:11:13 +0300

    changed structure

--- client/src/components/global/Navbar/Menu.tsx
+++ client/src/components/global/Navbar/Menu.tsx
@@ -2,31 +2,45 @@ import { Link, useLocation } from "react-router-dom";
 
 const Menu = () => {
   const bfLoginLinks = [
-    { label: "Login", path: "/login" },
-    { label: "Register", path: "/register" },
+    { label: "Join now", path: "/register" },
+    { label: "Sign in", path: "/login" },
   ];
   return (
-    <ul className="navbar-nav ms-auto">
-      {
-        bfLoginLinks.map((link, index) => (
-          <li key={index} className="nav-item">
-            <Link className="nav-link" to={link.path}>{link.label}</Link>
-          </li>
-        ))
-      }
+    <ul className='navbar-nav ms-auto'>
+      {bfLoginLinks.map((link, index) => (
+        <li key={index} className='nav-item'>
+          <Link className='nav-link' to={link.path}>
+            {link.label}
+          </Link>
+        </li>
+      ))}
 
-      <li className="nav-item dropdown">
-        <span className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <li className='nav-item dropdown'>
+        <span
+          className='nav-link dropdown-toggle'
+          id='navbarDropdown'
+          role='button'
+          data-bs-toggle='dropdown'
+          aria-expanded='false'>
           UserName
         </span>
 
-        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
-          <li><hr className="dropdown-divider" /></li>
-          <li><Link className="dropdown-item" to="/">Logout</Link></li>
+        <ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
+          <li>
+            <Link className='dropdown-item' to='/profile'>
+              Profile
+            </Link>
+          </li>
+          <li>
+            <hr className='dropdown-divider' />
+          </li>
+          <li>
+            <Link className='dropdown-item' to='/'>
+              Sign out
+            </Link>
+          </li>
         </ul>
       </li>
-
     </ul>
   );
 };
