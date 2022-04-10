c85d1ab0eddf625eeaf345c6c9a032dbfc90b7f5
Andrew <ansinelnikov@gmail.com>
2022-04-10 22:52:57 +0300

    added About page

---
+++ client/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NormalPageLayout from "../components/layouts/NormalPageLayout";
+
+const About = () => {
+  return (
+    <NormalPageLayout>
+      <h2>About</h2>
+    </NormalPageLayout>
+  );
+};
+
+export default About;
