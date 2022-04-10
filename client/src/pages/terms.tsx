80273c679b6f7109aab629d7d9aa8cd583d7b50a
Andrew <ansinelnikov@gmail.com>
2022-04-10 23:06:53 +0300

    added Terms component

---
+++ client/src/pages/terms.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NormalPageLayout from "../components/layouts/NormalPageLayout";
+
+const Terms = () => {
+  return (
+    <NormalPageLayout>
+      <h2>Terms</h2>
+    </NormalPageLayout>
+  );
+};
+
+export default Terms;
