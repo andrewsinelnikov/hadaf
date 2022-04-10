255b6e93b40fc12988da5805e2470d2b200d3d58
Andrew <ansinelnikov@gmail.com>
2022-04-10 23:05:37 +0300

    added Privacy component

---
+++ client/src/pages/privacy.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NormalPageLayout from "../components/layouts/NormalPageLayout";
+
+const Privacy = () => {
+  return (
+    <NormalPageLayout>
+      <h2>Privacy</h2>
+    </NormalPageLayout>
+  );
+};
+
+export default Privacy;
