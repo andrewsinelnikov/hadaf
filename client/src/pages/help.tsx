9fd26f6c804657d436630350687beb0fb48e654e
Andrew <ansinelnikov@gmail.com>
2022-04-10 23:07:55 +0300

    added help component

---
+++ client/src/pages/help.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NormalPageLayout from "../components/layouts/NormalPageLayout";
+
+const Help = () => {
+  return (
+    <NormalPageLayout>
+      <h2>Help</h2>
+    </NormalPageLayout>
+  );
+};
+
+export default Help;
