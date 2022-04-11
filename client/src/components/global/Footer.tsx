8d800a2d3221efa5b29903640d79a625fae30a78
Andrew <ansinelnikov@gmail.com>
2022-04-11 22:45:28 +0300

    changed structure

--- client/src/components/global/Footer.tsx
+++ client/src/components/global/Footer.tsx
@@ -7,25 +7,27 @@ const Footer = () => {
         <ul className='footer-rights'>
           <li>&copy; {new Date().getFullYear()} Hadaf</li>
         </ul>
-        <ul className='footer-info'>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/privacy'>Privacy</Link>
-          </li>
-          <li>
-            <Link to='/terms'>Terms</Link>
-          </li>
-          <li>
-            <Link to='/help'>Help</Link>
-          </li>
-        </ul>
-        <form action='' className='footer-language'>
-          <select id='language' name='language'>
-            <option value='english'>English</option>
-          </select>
-        </form>
+        <div className='footer-info'>
+          <ul className='footer-left'>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/privacy'>Privacy</Link>
+            </li>
+            <li>
+              <Link to='/terms'>Terms</Link>
+            </li>
+            <li>
+              <Link to='/help'>Help</Link>
+            </li>
+          </ul>
+          <form action='' className='footer-'>
+            <select id='language' name='language'>
+              <option value='english'>English</option>
+            </select>
+          </form>
+        </div>
       </div>
     </footer>
   );
