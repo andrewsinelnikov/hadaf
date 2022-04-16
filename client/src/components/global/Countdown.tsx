1c8cfac46b32d865c475196aae3855c9eef83f4a
Andrew <ansinelnikov@gmail.com>
2022-04-16 21:04:55 +0300

    changed structure

--- client/src/components/global/Countdown.tsx
+++ client/src/components/global/Countdown.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Countdown = () => {
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+  date.setHours(0, 0, 0);
+  const countDate = new Date(date).getTime();
+
   return (
     <div>
-        <h1>Countdown</h1>
+      <h1>Countdown</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Countdown
+export default Countdown;
