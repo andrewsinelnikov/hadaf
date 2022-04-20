d326125769be725c90e3765fa0febe054424704a
Andrew <ansinelnikov@gmail.com>
2022-04-20 11:17:03 +0300

    changed structure

--- client/src/components/global/NotFound.tsx
+++ client/src/components/global/NotFound.tsx
@@ -7,6 +7,8 @@ const NotFound = () => {
       <div className='notFound'>
         <h2>404 | Page Not Found</h2>
         <p>Sorry, we can’t seem to find the page you’re looking for.</p>
+        <p>Sorry, we can’t seem to find the page you’re looking for.</p>
+        <p>Sorry, we can’t seem to find the page you’re looking for.</p>
       </div>
     </ExceptionalPageLayout>
   );
