cd42bfb6ee0b4208cf1f7d8432df19333b10a713
Andrew <ansinelnikov@gmail.com>
2022-04-20 09:23:47 +0300

    defined types

--- client/src/components/global/Navbar/clickOutside.ts
+++ client/src/components/global/Navbar/clickOutside.ts
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import { RefObject, useEffect } from "react";
 
-export const useOnClickOutside = (
-  ref: React.RefObject<HTMLElement>,
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
   handler: (event: MouseEvent | TouchEvent) => void
 ) => {
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
-      if (!ref.current || ref.current.contains(event.target)) {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
       handler(event);
