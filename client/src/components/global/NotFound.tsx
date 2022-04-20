0a11ea6f6fcd5ed63983b6c6d5bf66472a2d3edf
Andrew <ansinelnikov@gmail.com>
2022-04-20 11:00:18 +0300

    changed structure

--- client/src/components/global/NotFound.tsx
+++ client/src/components/global/NotFound.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import ExceptionalPageLayout from "../layouts/ExceptionalPageLayout";
 
 const NotFound = () => {
   return (
-    <div className='notFound'>
-      <h2>404 | Not Found</h2>
-    </div>
+    <ExceptionalPageLayout>
+      <div className='notFound'>
+        <h2>404 | Page Not Found</h2>
+        <p>Sorry, we can’t seem to find the page you’re looking for.</p>
+      </div>
+    </ExceptionalPageLayout>
   );
 };
 
