d63d9d579d77c03cbb2ea3424797a20222dc7e76
Andrew <ansinelnikov@gmail.com>
2022-04-21 22:42:39 +0300

    changed structure

--- client/src/components/global/NotFound.tsx
+++ client/src/components/global/NotFound.tsx
@@ -8,7 +8,14 @@ const NotFound = () => {
         <h2>404 | Page Not Found</h2>
         <p>Sorry, we can’t seem to find the page you’re looking for.</p>
         <p> It may be temporarily unavailable, moved or no longer exist.</p>
-        <p>Sorry, we can’t seem to find the page you’re looking for.</p>
+        <p>
+          The link you followed may be broken, or the page may have been
+          removed.
+        </p>
+        <p>
+          The page you are looking for might have been removed, had its name
+          changed or is temporarily unavailable.
+        </p>
       </div>
     </ExceptionalPageLayout>
   );
