95cd6b12323e36d5338d6cc1b458c6b35c7a4386
Andrew <ansinelnikov@gmail.com>
2022-04-22 10:08:51 +0300

    changed structure

--- client/src/components/global/NotFound.tsx
+++ client/src/components/global/NotFound.tsx
@@ -8,29 +8,14 @@ const NotFound = () => {
         <h2>
           4<i className='far fa-question-circle fa-spin' />4 | Page not found
         </h2>
-        <p>Sorry, we can’t seem to find the page you’re looking for.</p>
-        <div>
-          Maybe the link you followed is broken? Or this page moved? Got
-          deleted? Is hiding out in quarantine? Had its name changed?
-        </div>
-        <p>
-          The link you followed may be broken, or the page may have been
-          removed.
-          <i className='far fa-question-circle fa-spin' />
-        </p>
-        <p>
-          The page you are looking for might have been removed, had its name
-          changed or is temporarily unavailable.
-        </p>
         <div className='msg'>
-          Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
-          existed in the first place?
-          <p>
-            Let's go <a href='#'>home</a> and try from there.
-          </p>
+          Sorry, we can’t seem to find the page you’re looking for.
         </div>
-        <p>Go back to Instagram or let us know about the trouble</p>
         <div>
+          The link you followed may be broken, or the page may have been
+          removed, had its name changed or is temporarily unavailable.
+        </div>
+        <div className='msg'>
           <Link to='/Home'>Home</Link>
           <Link to='/help'>Help</Link>
         </div>
