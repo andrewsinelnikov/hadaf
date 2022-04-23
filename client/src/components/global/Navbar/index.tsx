0c2af164106ee2265c5e56619fed8c21daad2e69
Andrew <ansinelnikov@gmail.com>
2022-04-23 22:02:24 +0300

    chaanged structure

--- client/src/components/global/Navbar/index.tsx
+++ client/src/components/global/Navbar/index.tsx
@@ -23,34 +23,6 @@ const Navbar = () => {
         <Menu openSearch={openSearch} setOpenSearch={setOpenSearch} />
       </div>
     </nav>
-    // <nav className='navbar'>
-    //   <div className='navbar-wrapper'>
-    //     <h1 className='navbar-left'>
-    //       <Link to='/' className='navbar-brand'>
-    //         <img
-    //           src='../../../../logo-gold2.png'
-    //           alt='logo'
-    //           className='navbar-logo'
-    //         />{" "}
-    //         HADAF
-    //       </Link>
-    //     </h1>
-    //     <button
-    //       className='navbar-toggler'
-    //       type='button'
-    //       data-bs-toggle='collapse'
-    //       data-bs-target='#navbarSupportedContent'
-    //       aria-controls='navbarSupportedContent'
-    //       aria-expanded='false'
-    //       aria-label='Toggle navigation'>
-    //       <span className='navbar-togglerIcon'></span>
-    //     </button>
-    //     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-    //       <Search />
-    //       <Menu />
-    //     </div>
-    //   </div>
-    // </nav>
   );
 };
 
