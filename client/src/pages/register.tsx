367b6bbb044015c89000bcc94743022f9abdc13a
Andrew <ansinelnikov@gmail.com>
2022-04-23 20:51:23 +0300

    changed structure

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -2,25 +2,28 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import RegisterForm from "../components/auth/RegisterForm";
+import NormalPageLayout from "../components/layouts/NormalPageLayout";
 
 const Register = () => {
   const location = useLocation();
 
   return (
-    <div className='auth_page'>
-      <div className='auth_box'>
-        <h3 className='text-uppercase text-center mb-4'>Register</h3>
-        <RegisterForm />
+    <NormalPageLayout>
+      <div className='auth_page'>
+        <div className='auth_box'>
+          <h3 className='text-uppercase text-center mb-4'>Register</h3>
+          <RegisterForm />
 
-        <p className='mt-2'>
-          {`Already have an account? `}
-          {/* <Link to={`/login${location.search}`} style={{ color: "crimson" }}> */}
-          <Link to={`/login}`} style={{ color: "crimson" }}>
-            Login
-          </Link>
-        </p>
+          <p className='mt-2'>
+            {`Already have an account? `}
+            {/* <Link to={`/login${location.search}`} style={{ color: "crimson" }}> */}
+            <Link to={`/login}`} style={{ color: "crimson" }}>
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </NormalPageLayout>
   );
 };
 
