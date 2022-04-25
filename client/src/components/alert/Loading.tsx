caef2cc7faabf1ff7d217f77a72fcfe86ae26e16
Andrew <ansinelnikov@gmail.com>
2022-04-25 19:34:11 +0300

    changed structure

--- client/src/components/alert/Loading.tsx
+++ client/src/components/alert/Loading.tsx
@@ -8,15 +8,10 @@ const Loading = () => {
         <line className='loader-line' x1='125' y1='125' x2='143' y2='125' />
         <circle className='loader-circle' cx='125' cy='125' r='35' />
         <circle className='loader-center' cx='125' cy='125' r='5' />
-        <text fill='var(--lightmiddle-color)' x='97' y='190'>
+        <text className='loader-logo' x='95' y='190'>
           Hadaf
         </text>
       </svg>
-      {/* <svg width='250' height='50'>
-        <text fill='#e7e0b9' x='5' y='47'>
-          Hadaf
-        </text>
-      </svg> */}
     </div>
   );
 };
