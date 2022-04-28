a7e2dc561507c6a39801a8bb0082a6d0de7269bf
Andrew <ansinelnikov@gmail.com>
2022-04-28 10:08:37 +0300

    changed structure

--- client/src/components/alert/Toast.tsx
+++ client/src/components/alert/Toast.tsx
@@ -15,27 +15,29 @@ const Toast = ({ title, body, bgColor }: IProps) => {
   };
 
   return (
-    <div className={`toast show`} style={{ background: `${bgColor}` }}>
-      <div className='toast-header' style={{ background: `${bgColor}` }}>
-        <strong>{title}</strong>
-        <button
-          type='button'
-          className='btn-close'
-          data-bs-dismiss='toast'
-          aria-label='Close'
-          onClick={handleClose}
-        />
-      </div>
-      <div className='toast-body'>
-        {typeof body === "string" ? (
-          body
-        ) : (
-          <ul>
-            {body.map((text, index) => (
-              <li key={index}>{text}</li>
-            ))}
-          </ul>
-        )}
+    <div className='toast'>
+      <div className={`toast-box show`} style={{ background: `${bgColor}` }}>
+        <div className='toast-header' style={{ background: `${bgColor}` }}>
+          <strong>{title}</strong>
+          <button
+            type='button'
+            className='btn-close'
+            data-bs-dismiss='toast'
+            aria-label='Close'
+            onClick={handleClose}
+          />
+        </div>
+        <div className='toast-body'>
+          {typeof body === "string" ? (
+            body
+          ) : (
+            <ul>
+              {body.map((text, index) => (
+                <li key={index}>{text}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
