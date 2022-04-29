b30ad2cdcbb2da54c33b05c2dfb552d7bced4ab9
Andrew <ansinelnikov@gmail.com>
2022-04-29 20:25:47 +0300

    added points

--- client/src/components/global/Countdown.tsx
+++ client/src/components/global/Countdown.tsx
@@ -20,7 +20,15 @@ const Countdown = () => {
       const currentTime = new Date().getTime();
       const gap = countdownDate - currentTime;
 
-      let days = Math.floor(gap / (1000 * 60 * 60 * 24));
+      let second = 1000;
+      let minute = second * 60;
+      let hour = minute * 60;
+      let day = hour * 24;
+
+      let days = Math.floor(gap / day);
+      let hours = Math.floor((gap % day) / hour);
+      let minutes = Math.floor((gap % hour) / minute);
+      let seconds = Math.floor((gap % minute) / second);
     }
   };
 
