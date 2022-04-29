6bb949625662c2fe907450fbab392415faaab0d0
Andrew <ansinelnikov@gmail.com>
2022-04-29 19:58:53 +0300

    added left state

--- client/src/components/global/Countdown.tsx
+++ client/src/components/global/Countdown.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Countdown = () => {
-  const date = new Date();
-  date.setDate(date.getDate() + 1);
-  date.setHours(0, 0, 0);
-  const countDate = new Date(date).getTime();
+  const [left, setLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+  // const date = new Date();
+  // date.setDate(date.getDate() + 1);
+  // date.setHours(0, 0, 0);
+  // const countDate = new Date(date).getTime();
 
   return (
     <div>
