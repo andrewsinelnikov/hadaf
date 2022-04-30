33092fa5b84dcf2fa99560d8608ce9b625fa5f71
Andrew <ansinelnikov@gmail.com>
2022-04-30 22:39:57 +0300

    added 0

--- client/src/components/global/Countdown.tsx
+++ client/src/components/global/Countdown.tsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 
 const Countdown = () => {
   const [countdownDate, setCountdownDate] = useState(
-    new Date("5/1/2022").getTime()
+    new Date("5/2/2022").getTime()
   );
   const [left, setLeft] = useState({
-    days: "",
-    hours: "",
-    minutes: "",
-    seconds: "",
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
   });
   // const date = new Date();
   // date.setDate(date.getDate() + 1);
@@ -31,20 +31,20 @@ const Countdown = () => {
       let hour = minute * 60;
       let day = hour * 24;
 
-      let days = Math.floor(gap / day).toString();
-      let hours = Math.floor((gap % day) / hour).toString();
-      let minutes = Math.floor((gap % hour) / minute).toString();
-      let seconds = Math.floor((gap % minute) / second).toString();
+      let days = Math.floor(gap / day);
+      let hours = Math.floor((gap % day) / hour);
+      let minutes = Math.floor((gap % hour) / minute);
+      let seconds = Math.floor((gap % minute) / second);
 
-      const numbersToAddZeroTo = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-      days = `${days === "0" ? "" : days}`;
-      if (numbersToAddZeroTo.includes(hours)) {
-        hours = `0${hours}`;
-      } else if (numbersToAddZeroTo.includes(minutes)) {
-        minutes = `0${minutes}`;
-      } else if (numbersToAddZeroTo.includes(seconds)) {
-        seconds = `0${seconds}`;
-      }
+      // const numbersToAddZeroTo = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      // days = `${days === "0" ? "" : days}`;
+      // if (numbersToAddZeroTo.includes(hours)) {
+      //   hours = `0${hours}`;
+      // } else if (numbersToAddZeroTo.includes(minutes)) {
+      //   minutes = `0${minutes}`;
+      // } else if (numbersToAddZeroTo.includes(seconds)) {
+      //   seconds = `0${seconds}`;
+      // }
 
       setLeft({ days, hours, minutes, seconds });
     }
@@ -52,7 +52,9 @@ const Countdown = () => {
 
   return (
     <div>
-      {left.days ? `${left.days}:` : ""}
+      {/* {left.days ? `${left.days}:` : ""} */}
+      {left.days ? (left.days > 10 ? `${left.days}:` : `0${left.days}:`) : ""}
+      {/* {left.hours || "00"}:{left.minutes || "00"}:{left.seconds || "00"} */}
       {left.hours || "00"}:{left.minutes || "00"}:{left.seconds || "00"}
     </div>
   );
