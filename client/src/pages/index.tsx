4fed613f6b112e9573fdaa931d336f2044b509dc
Andrew <ansinelnikov@gmail.com>
2022-05-01 12:48:10 +0300

    added endOfDay()

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,9 +3,11 @@ import Countdown from "../components/global/Countdown";
 import ExceptionalPageLayout from "../components/layouts/ExceptionalPageLayout";
 
 const Home = () => {
-  const endOfDay = (date: Date) => {
-    let lastday = date.getDate() - (date.getDay() - 1) + 6;
-    return new Date(date.setDate(lastday));
+  const endOfDay = () => {
+    let date = new Date();
+    date.setDate(date.getDate() + 1);
+    date.setHours(0, 0, 0);
+    return new Date(date);
   };
 
   const endOfWeek = (date: Date) => {
@@ -17,8 +19,8 @@ const Home = () => {
   return (
     <ExceptionalPageLayout>
       <h2>Home page</h2>
-      {/* <Countdown date={new Date("05/01/2022")} /> */}
-      <Countdown date={endOfWeek(new Date())} />
+      <Countdown date={endOfDay()} />
+      {/* <Countdown date={endOfWeek(new Date())} /> */}
     </ExceptionalPageLayout>
   );
 };
