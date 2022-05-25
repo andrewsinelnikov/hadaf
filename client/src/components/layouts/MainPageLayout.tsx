51449b3f4748fc0a551879b3131af193f10161b2
Andrew <ansinelnikov@gmail.com>
2022-05-25 22:45:56 +0300

    added IProps

--- client/src/components/layouts/MainPageLayout.tsx
+++ client/src/components/layouts/MainPageLayout.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Navbar from "../global/Navbar";
 
-const MainPageLayout: React.FC<{}> = ({ children }) => {
+interface IProps {
+  bg?: String;
+  children?: React.ReactNode;
+}
+
+const MainPageLayout = ({ bg, children }: IProps) => {
   // https://stackoverflow.com/questions/65605219/rendre-footer-component-all-page-except-404-pages-in-react
 
   return (
     <>
       <Navbar />
-      <div className='container'>{children}</div>
+      <div className='container' style={{ background: `${bg}` }}>
+        {children}
+      </div>
     </>
   );
 };
