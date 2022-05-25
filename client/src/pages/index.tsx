17aeecb0b4b24f0db9adebabf7d965a6cf5c54af
Andrew <ansinelnikov@gmail.com>
2022-05-25 21:57:28 +0300

    changed structure

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,48 +1,8 @@
-import React from "react";
-import Countdown from "../components/global/Countdown";
 import ExceptionalPageLayout from "../components/layouts/ExceptionalPageLayout";
-import { endOfDay, endOfWeek } from "../utils/FindEnd";
+import Countdown from "../components/global/Countdown";
+import { endOfDay, endOfWeek, endOfSeason } from "../utils/FindEnd";
 
 const Home = () => {
-  const endOfSeason = () => {
-    let date = new Date();
-    if (date.getDay() === 0) return endOfDay();
-    switch (date.getMonth()) {
-      case 0:
-      case 1:
-      case 12:
-        date.setDate(1);
-        date.setMonth(2);
-        date.setHours(0, 0, 0);
-        break;
-      case 2:
-      case 3:
-      case 4:
-        date.setDate(1);
-        date.setMonth(5);
-        date.setHours(0, 0, 0);
-        break;
-      case 5:
-      case 6:
-      case 7:
-        date.setDate(1);
-        date.setMonth(8);
-        date.setHours(0, 0, 0);
-        break;
-      case 8:
-      case 9:
-      case 10:
-        date.setDate(1);
-        date.setMonth(11);
-        date.setHours(0, 0, 0);
-        break;
-    }
-
-    return new Date(date);
-  };
-
-  // console.log(endOfWeek());
-  // console.log(endOfSeason());
   return (
     <ExceptionalPageLayout>
       <h2>Home page</h2>
