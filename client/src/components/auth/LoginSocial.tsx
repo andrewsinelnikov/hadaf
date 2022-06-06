2427ac47e8d0bbc9b59211cb2f405d5523c5e7a3
Andrew <ansinelnikov@gmail.com>
2022-06-06 23:23:01 +0300

    added GoogleLogin

--- client/src/components/auth/LoginSocial.tsx
+++ client/src/components/auth/LoginSocial.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import { GoogleLogin, GoogleLoginResponse } from "react-google-login-lite";
+import {
+  FacebookLogin,
+  FacebookLoginAuthResponse,
+} from "react-facebook-login-lite";
+
+// import { googleLogin } from "../../redux/actions/authAction";
 
 const LoginSocial = () => {
-  return <div>LoginSocial</div>;
+  const dispatch = useDispatch();
+
+  const onSuccess = (googleUser: GoogleLoginResponse) => {
+    const id_token = googleUser.getAuthResponse().id_token;
+    // dispatch(googleLogin(id_token));
+  };
+
+  return (
+    <>
+      <div className='auth_social'>
+        <GoogleLogin
+          client_id='740324844325-d3jsrbijikpeic8pi1dmrnpnsved1chf.apps.googleusercontent.com'
+          cookiepolicy='single_host_origin'
+          onSuccess={onSuccess}
+          theme='light'
+        />
+      </div>
+    </>
+  );
 };
 
 export default LoginSocial;
