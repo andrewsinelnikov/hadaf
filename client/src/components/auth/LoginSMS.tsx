0e7bff64aba4a7d86d306644f6a8074efced6a3d
Andrew <ansinelnikov@gmail.com>
2022-06-07 10:44:46 +0300

    added handleSubmit

--- client/src/components/auth/LoginSMS.tsx
+++ client/src/components/auth/LoginSMS.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { FormSubmit } from "../../utils/TypeScript";
+// import { loginSMS } from "../../redux/actions/authAction";
 
 const LoginSMS = () => {
   const [phone, setPhone] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: FormSubmit) => {
+    e.preventDefault();
+    // dispatch(loginSMS(phone));
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='form-group'>
         {/* <label htmlFor='phone' className='form-label'>
           Phone number
