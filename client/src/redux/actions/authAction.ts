8e06c7f52d093741de6c56d0f8f7525c03fc01b0
Andrew <ansinelnikov@gmail.com>
2022-06-07 09:12:16 +0300

    added facebookLogin

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -87,3 +87,19 @@ export const googleLogin =
       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
     }
   };
+
+export const facebookLogin =
+  (accessToken: string, userID: string) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+      const res = await postAPI("facebook_login", { accessToken, userID });
+
+      dispatch({ type: AUTH, payload: res.data });
+
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+      localStorage.setItem("logged", "hadaf");
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
