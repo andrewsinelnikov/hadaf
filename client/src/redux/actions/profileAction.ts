93335d3f942bb6dfa8a9d814b1f60a8257c82c56
Andrew <ansinelnikov@gmail.com>
2022-06-08 15:53:55 +0300

    added updateUser action

---
+++ client/src/redux/actions/profileAction.ts
@@ -0,0 +1,51 @@
+import { Dispatch } from "redux";
+import { AUTH, IAuth, IAuthType } from "../types/authType";
+import { ALERT, IAlertType } from "../types/alertType";
+import { checkImage, imageUpload } from "../../utils/ImageUpload";
+
+export const updateUser =
+  (avatar: Blob, name: string, auth: IAuth) =>
+  async (dispatch: Dispatch<IAlertType | IAuthType>) => {
+    if (!auth.access_token || !auth.user) return;
+    let url = "";
+
+    // const result = await checkTokenExp(auth.access_token, dispatch);
+    // const access_token = result ? result : auth.access_token;
+
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      if (avatar) {
+        const check = checkImage(avatar);
+        if (check) return dispatch({ type: ALERT, payload: { errors: check } });
+
+        const photo = await imageUpload(avatar);
+        url = photo.url;
+      }
+
+      //   dispatch({
+      //     type: AUTH,
+      //     payload: {
+      //       access_token: auth.access_token,
+      //       user: {
+      //         ...auth.user,
+      //         avatar: url ? url : auth.user.avatar,
+      //         name: name ? name : auth.user.name,
+      //       },
+      //     },
+      //   });
+
+      //   const res = await patchAPI(
+      //     "user",
+      //     {
+      //       avatar: url ? url : auth.user.avatar,
+      //       name: name ? name : auth.user.name,
+      //     },
+      //     access_token
+      //   );
+
+      dispatch({ type: ALERT, payload: { loading: false } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
