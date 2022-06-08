d74288dfc6077b956a4ff671200425a3ff564aba
Andrew <ansinelnikov@gmail.com>
2022-06-08 14:01:05 +0300

    added Profile page

---
+++ client/src/pages/profile/[slug].tsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { IParams, RootStore } from "../../utils/TypeScript";
+import UserInfo from "../../components/profile/UserInfo";
+import OtherInfo from "../../components/profile/OtherInfo";
+import UserPosts from "../../components/profile/UserPosts";
+
+const Profile = () => {
+  const { slug }: IParams = useParams<keyof IParams>() as IParams;
+  const { auth } = useSelector((state: RootStore) => state);
+
+  return (
+    <div>
+      <div>{auth.user?._id === slug ? <UserInfo /> : <OtherInfo />}</div>
+      <div>
+        <UserPosts />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
