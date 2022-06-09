da68500a9ad455fc8c315d326f8d1d862c07b7b6
Andrew <ansinelnikov@gmail.com>
2022-06-09 13:49:09 +0300

    added userCtrl

---
+++ server/controllers/userCtrl.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+import { IReqAuth } from "../config/interface";
+import bcrypt from "bcrypt";
+
+const userCtrl = {};
+
+export default userCtrl;
