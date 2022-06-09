4c8f514f39b9ea11b9c5113526e92ffd7a06607b
Andrew <ansinelnikov@gmail.com>
2022-06-09 13:49:57 +0300

    added updateUser controller

--- server/controllers/userCtrl.ts
+++ server/controllers/userCtrl.ts
@@ -3,6 +3,21 @@ import User from "../models/userModel";
 import { IReqAuth } from "../config/interface";
 import bcrypt from "bcrypt";
 
-const userCtrl = {};
+const userCtrl = {
+  updateUser: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ msg: "Invalid Authentication" });
+
+    try {
+      const { avatar, name } = req.body;
+
+      await User.findOneAndUpdate({ _id: req.user._id }, { avatar, name });
+
+      res.json({ msg: "Update Success" });
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
 
 export default userCtrl;
