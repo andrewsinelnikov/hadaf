2a6424dd7a17a792603bdfce784eaf5de4a53999
Andrew <ansinelnikov@gmail.com>
2022-06-11 22:10:45 +0300

    added resetPassword

--- server/controllers/userCtrl.ts
+++ server/controllers/userCtrl.ts
@@ -18,7 +18,29 @@ const userCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
-  resetPassword: async (req: IReqAuth, res: Response) => {},
+  resetPassword: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ msg: "Invalid Authentication" });
+
+    if (req.user.type !== "register")
+      return res.status(400).json({
+        msg: `Quick login account with ${req.user.type} can't use this function`,
+      });
+
+    try {
+      const { password } = req.body;
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { password: passwordHash }
+      );
+
+      res.json({ msg: "Reset Password Success!" });
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 export default userCtrl;
