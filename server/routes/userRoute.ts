065ea9bd089b2c23b49bd4845b6e71e8bfb491df
Andrew <ansinelnikov@gmail.com>
2022-06-11 22:12:40 +0300

    added Reset Password

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -9,4 +9,9 @@ const router = express.Router();
 // @access Private
 router.patch("/user", auth, userCtrl.updateUser);
 
+// @route  PATCH api/reset_password
+// @desc   Reset Password
+// @access Private
+router.patch("/reset_password", auth, userCtrl.resetPassword);
+
 export default router;
