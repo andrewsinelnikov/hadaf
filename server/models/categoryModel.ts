1a57691a1b766c865c516fb0e5907036a2291fa5
Andrew <ansinelnikov@gmail.com>
2022-06-12 09:01:08 +0300

    added Category model

---
+++ server/models/categoryModel.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add new category"],
+      trim: true,
+      unique: true,
+      maxLength: [50, "Name is up to 50 chars long"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Category", categorySchema);
