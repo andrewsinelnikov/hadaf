e08d010720b949cc258be2f27e1a97b93c8eb4ff
Andrew <ansinelnikov@gmail.com>
2022-06-14 22:55:01 +0300

    added deleteAPI

--- client/src/utils/FetchData.ts
+++ client/src/utils/FetchData.ts
@@ -31,3 +31,11 @@ export const putAPI = async (url: string, post: object, token?: string) => {
 
   return res;
 };
+
+export const deleteAPI = async (url: string, token?: string) => {
+  const res = await axios.delete(`/api/${url}`, {
+    headers: { Authorization: token as string },
+  });
+
+  return res;
+};
