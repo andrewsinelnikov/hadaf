47d248cfc63e20c09ba218680ed74e332504f972
Andrew <ansinelnikov@gmail.com>
2023-05-28 09:34:42 +0300

    added action

--- client/src/PageRender.tsx
+++ client/src/PageRender.tsx
@@ -14,12 +14,16 @@ const generatePage = (name: string) => {
 };
 
 const PageRender = () => {
-  const { page, slug }: IParams = useParams<keyof IParams>() as IParams;
+  const { page, slug, action }: IParams = useParams<keyof IParams>() as IParams;
 
   let name = "";
 
   if (page) {
-    name = slug ? `${page}/[slug]` : `${page}`;
+    if (action) {
+      name = `${page}/[slug]/[action]`;
+    } else {
+      name = slug ? `${page}/[slug]` : `${page}`;
+    }
   }
   return generatePage(name);
 };
