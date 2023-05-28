02c686c6d5807588497d57e4cc6bf22ae9c9634e
Andrew <ansinelnikov@gmail.com>
2023-05-28 21:53:16 +0300

    added handleSubmit()

--- client/src/components/profile/UserEdit.tsx
+++ client/src/components/profile/UserEdit.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { FormSubmit } from "../../utils/TypeScript";
 
 const UserEdit = () => {
+  let navigate = useNavigate();
+
+  const handleSubmit = (e: FormSubmit) => {
+    e.preventDefault();
+    navigate(-1);
+  };
+
   return (
     <div className='edit-box'>
       <h3 className='edit-title'>Edit profile</h3>
+      <form className='edit-info' onSubmit={handleSubmit}></form>
     </div>
   );
 };
