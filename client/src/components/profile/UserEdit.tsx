cf73d2c4ef63e293768d198b21f2fef1cc74f138
Andrew <ansinelnikov@gmail.com>
2023-05-28 21:57:46 +0300

    added handleChangeInput()

--- client/src/components/profile/UserEdit.tsx
+++ client/src/components/profile/UserEdit.tsx
@@ -1,10 +1,28 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { FormSubmit } from "../../utils/TypeScript";
+import { FormSubmit, IUserProfile, InputChange } from "../../utils/TypeScript";
 
 const UserEdit = () => {
   let navigate = useNavigate();
+
+  const initState = {
+    name: "",
+    account: "",
+    image: "",
+    password: "",
+    cf_password: "",
+  };
+
+  const [user, setUser] = useState<IUserProfile>(initState);
+  const [typePass, setTypePass] = useState(false);
+  const [typeCfPass, setTypeCfPass] = useState(false);
+  const { name, image, password, cf_password } = user;
+
+  const handleChangeInput = (e: InputChange) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
 
   const handleSubmit = (e: FormSubmit) => {
     e.preventDefault();
