b579b3e368128507c753c7c99aeac28369af7e28
Andrew <ansinelnikov@gmail.com>
2023-05-28 09:54:27 +0300

    added UserEdit component

--- client/src/pages/profile/[slug].tsx
+++ client/src/pages/profile/[slug].tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 import { IParams, RootStore } from "../../utils/TypeScript";
 import UserLayout from "../../components/layouts/UserLayout";
+import Layout from "../../components/layouts/Layout";
 import UserInfo from "../../components/profile/UserInfo";
 import UserEdit from "../../components/profile/UserEdit";
 import OtherInfo from "../../components/profile/OtherInfo";
@@ -12,32 +13,20 @@ const Profile = () => {
   const { slug, action }: IParams = useParams<keyof IParams>() as IParams;
   const { auth } = useSelector((state: RootStore) => state);
 
+  if (auth.user?._id === slug && action === "edit")
+    return (
+      <Layout navbarType={1}>
+        <UserEdit />
+      </Layout>
+    );
+
   return (
     <UserLayout navbarType={1}>
       <div className='profile'>
-        {/* {auth.user?._id === slug ? <UserInfo /> : <OtherInfo />} */}
-        {auth.user?._id === slug ? (
-          action === "edit" ? (
-            <UserEdit />
-          ) : (
-            <>
-              <UserInfo />
-              <div className='profile-content'>
-                <UserPosts />
-              </div>
-            </>
-          )
-        ) : (
-          <>
-            <OtherInfo />
-            <div className='profile-content'>
-              <UserPosts />
-            </div>
-          </>
-        )}
-        {/* <div className='profile-content'>
+        {auth.user?._id === slug ? <UserInfo /> : <OtherInfo />}
+        <div className='profile-content'>
           <UserPosts />
-        </div> */}
+        </div>
       </div>
     </UserLayout>
   );
