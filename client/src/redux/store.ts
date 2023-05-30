0b7e567939c4e7fb08626d7eb7b3033a295ec105
Andrew <ansinelnikov@gmail.com>
2023-05-30 14:42:50 +0300

    updated redux

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 // import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
