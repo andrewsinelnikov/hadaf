7f802cc215b9dadce90e5cd0c6744ad43601d0a0
Andrew <ansinelnikov@gmail.com>
2023-06-01 20:05:28 +0300

    added TimeReminder structure

--- client/src/components/workboard/TimeReminder.tsx
+++ client/src/components/workboard/TimeReminder.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useState } from "react";
 
 const TimeReminder = () => {
+  const [active, setActive] = useState();
   return (
     <div>
-        <h2>TimeReminder</h2>
+      <button className={active === "day" ? "time-active" : undefined}>
+        Day
+      </button>
+      <button className={active === "week" ? "time-active" : undefined}>
+        Week
+      </button>
+      <button className={active === "season" ? "time-active" : undefined}>
+        Season
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default TimeReminder
+export default TimeReminder;
