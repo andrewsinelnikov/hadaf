e18fe0552e5df18650bceae129c9d41b8a1f4654
Andrew <ansinelnikov@gmail.com>
2023-06-05 17:42:56 +0300

    added comps

--- client/src/pages/goals.tsx
+++ client/src/pages/goals.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { RootState } from "../redux/store";
+import { useAppSelector } from "../utils/hooks";
+import UserLayout from "../components/layouts/UserLayout";
+import UserInfo from "../components/profile/UserInfo";
+import TimeReminder from "../components/workboard/TimeReminder";
+import AddItem from "../components/workboard/AddItem";
 
 const Goals = () => {
+  const { auth } = useAppSelector((state: RootState) => state);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.access_token) navigate("/login");
+  }, [auth.access_token, navigate]);
+
+  const [item, setItem] = useState<string>("");
+
   return (
     <div>
       <h2>Goals</h2>
