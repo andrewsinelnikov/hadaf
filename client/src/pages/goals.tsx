97b9a49e9e4d58f48b0f24cfdc4dc77ea6065dc7
Andrew <ansinelnikov@gmail.com>
2023-06-08 21:55:35 +0300

    styled .items p

--- client/src/pages/goals.tsx
+++ client/src/pages/goals.tsx
@@ -20,22 +20,22 @@ const Goals = () => {
     case 0:
     case 1:
     case 12:
-      season = "winter";
+      season = "Winter";
       break;
     case 2:
     case 3:
     case 4:
-      season = "spring";
+      season = "Spring";
       break;
     case 5:
     case 6:
     case 7:
-      season = "summer";
+      season = "Summer";
       break;
     case 8:
     case 9:
     case 10:
-      season = "autumn";
+      season = "Autumn";
       break;
   }
 
