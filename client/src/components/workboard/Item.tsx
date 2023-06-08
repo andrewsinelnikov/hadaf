25b614164749faa32253e42a9e955398598e30ed
Andrew <ansinelnikov@gmail.com>
2023-06-08 18:44:35 +0300

    added IProps interface

--- client/src/components/workboard/Item.tsx
+++ client/src/components/workboard/Item.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { IItem } from "../../utils/TypeScript";
 
+interface IProps {
+  item: IItem;
+  items: IItem[];
+  setItems: React.Dispatch<React.SetStateAction<IItem[]>>;
+}
 
-
-const Item = ({ item, items, setItems }) => {
+const Item: React.FC<IProps> = ({ item, items, setItems }) => {
   return (
     <div>
       <h2>Item</h2>
