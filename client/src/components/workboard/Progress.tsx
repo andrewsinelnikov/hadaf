f4d3a2a7aa88329191badc0a273ac1b9e11ed687
Andrew <ansinelnikov@gmail.com>
2023-06-10 16:38:13 +0300

    styled .progress-bar

--- client/src/components/workboard/Progress.tsx
+++ client/src/components/workboard/Progress.tsx
@@ -7,7 +7,10 @@ interface IProps {
 const Progress: React.FC<IProps> = ({ completed }) => {
   return (
     <div className='progress'>
-      <div className='progress-filler' style={{ width: `${completed}%` }} />
+      <div className='progress-bar'>
+        <div className='progress-filler' style={{ width: `${completed}%` }} />
+      </div>
+      <span className='item-completeness'>{completed} %</span>
     </div>
   );
 };
