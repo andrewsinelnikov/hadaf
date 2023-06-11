bb8f89d0072c4b7608ba5586f4abbba1e290b84d
Andrew <ansinelnikov@gmail.com>
2023-06-11 21:00:38 +0300

    added navigationon

--- client/src/pages/journal.tsx
+++ client/src/pages/journal.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { RootState } from "../redux/store";
+import { useAppSelector } from "../utils/hooks";
+import UserLayout from "../components/layouts/UserLayout";
 
 const Journal = () => {
+  const { auth } = useAppSelector((state: RootState) => state);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.access_token) navigate("/login");
+  }, [auth.access_token, navigate]);
+
   return (
     <div>
       <h2>Journal</h2>
