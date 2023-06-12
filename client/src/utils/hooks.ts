e0d53ec9e82dfc17421d5554b901db353aa44a55
Andrew <ansinelnikov@gmail.com>
2023-06-12 21:08:51 +0300

    added useAutosizeTextarea

--- client/src/utils/hooks.ts
+++ client/src/utils/hooks.ts
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "../redux/store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAutosizeTextarea = (
+  textAreaRef: HTMLTextAreaElement | null,
+  value: string
+) => {
+  useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.style.height = "0px";
+      const scrollHeight = textAreaRef.scrollHeight;
+      textAreaRef.style.height = scrollHeight + "px";
+    }
+  }, [textAreaRef, value]);
+};
