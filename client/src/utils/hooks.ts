a2d91fe863aabaeba3628b43778deb6fb3267b8c
Andrew <ansinelnikov@gmail.com>
2023-06-12 23:46:45 +0300

    tested

--- client/src/utils/hooks.ts
+++ client/src/utils/hooks.ts
@@ -15,5 +15,11 @@ export const useAutosizeTextarea = (
       const scrollHeight = textAreaRef.scrollHeight;
       textAreaRef.style.height = scrollHeight + "px";
     }
+    if (textAreaRef && textAreaRef.clientHeight > window.innerHeight * 0.5) {
+      textAreaRef.style.overflow = "hidden auto";
+    }
+    // if (textAreaRef!.clientHeight > window.innerHeight * 0.5) {
+    //   textAreaRef!.style.overflow = "hidden auto";
+    // }
   }, [textAreaRef, value]);
 };
