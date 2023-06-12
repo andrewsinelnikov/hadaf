6b658039c6706568e2c1907d89be0584b7cf2f07
Andrew <ansinelnikov@gmail.com>
2023-06-12 21:24:03 +0300

    added count

--- client/src/components/workboard/AreaInput.tsx
+++ client/src/components/workboard/AreaInput.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import { useAutosizeTextarea } from "../../utils/hooks";
 
 interface IProps {
   note: string;
@@ -8,6 +9,8 @@ interface IProps {
 const AreaInput: React.FC<IProps> = ({ note, setNote }) => {
   const [isFocused, setIsFocused] = useState(false);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useAutosizeTextarea(textAreaRef.current, note);
   return (
     <form className='add-area'>
       <div className='add-item'>
@@ -28,6 +31,21 @@ const AreaInput: React.FC<IProps> = ({ note, setNote }) => {
             }`,
           }}
         />
+        {isFocused && note.length > 0 && (
+          <small style={{ fontWeight: "bold" }}>
+            <span
+              style={{
+                color: `${
+                  note.length > 2000
+                    ? "var(--error-color)"
+                    : "var(--done-color)"
+                }`,
+              }}>
+              {note.length}
+            </span>{" "}
+            / 2000
+          </small>
+        )}
       </div>
       <div className='item-options'>
         {note.length > 0 && (
