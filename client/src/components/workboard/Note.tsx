160ec729989ec6ac0b07a72ee2603a3d3c51d46b
Andrew <ansinelnikov@gmail.com>
2023-06-14 09:33:26 +0300

    added IProps interface

--- client/src/components/workboard/Note.tsx
+++ client/src/components/workboard/Note.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { IJournalNote } from "../../utils/TypeScript";
 
-const Note = () => {
+interface IProps {
+  note: IJournalNote;
+  notes: IJournalNote[];
+  setNotes: React.Dispatch<React.SetStateAction<IJournalNote[]>>;
+}
+
+const Note: React.FC<IProps> = ({ note, notes, setNotes }) => {
   return (
     <div>
       <h2>Note</h2>
