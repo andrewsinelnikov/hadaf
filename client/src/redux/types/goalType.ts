0a7f388439a1c00bce467601fc3a18b82276e070
Andrew <ansinelnikov@gmail.com>
2023-06-15 20:18:58 +0300

    added IGoalType

--- client/src/redux/types/goalType.ts
+++ client/src/redux/types/goalType.ts
@@ -1,32 +1,31 @@
-import { ICategory } from "../../utils/TypeScript";
+import { IItem } from "../../utils/TypeScript";
 
-export const CREATE_CATEGORY = "CREATE_CATEGORY";
-export const GET_CATEGORIES = "GET_CATEGORIES";
-export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
-export const DELETE_CATEGORY = "DELETE_CATEGORY";
+export const CREATE_GOAL = "CREATE_GOAL";
+// export const GET_CATEGORIES = "GET_CATEGORIES";
+// export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+// export const DELETE_CATEGORY = "DELETE_CATEGORY";
 
-export interface ICreateCategory {
-  type: typeof CREATE_CATEGORY;
-  payload: ICategory;
+export interface ICreateGoal {
+  type: typeof CREATE_GOAL;
+  payload: IItem;
 }
 
-export interface IGetCategories {
-  type: typeof GET_CATEGORIES;
-  payload: ICategory[];
-}
+// export interface IGetCategories {
+//   type: typeof GET_CATEGORIES;
+//   payload: ICategory[];
+// }
 
-export interface IUpdateCategory {
-  type: typeof UPDATE_CATEGORY;
-  payload: ICategory;
-}
+// export interface IUpdateCategory {
+//   type: typeof UPDATE_CATEGORY;
+//   payload: ICategory;
+// }
 
-export interface IDeleteCategory {
-  type: typeof DELETE_CATEGORY;
-  payload: string;
-}
+// export interface IDeleteCategory {
+//   type: typeof DELETE_CATEGORY;
+//   payload: string;
+// }
 
-export type ICategoryType =
-  | ICreateCategory
-  | IGetCategories
-  | IUpdateCategory
-  | IDeleteCategory;
+export type IGoalType = ICreateGoal;
+// | IGetCategories
+// | IUpdateCategory
+// | IDeleteCategory;
