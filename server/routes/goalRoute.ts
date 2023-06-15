b313d0344221310edc5bc9b97e812d0fd646da21
Andrew <ansinelnikov@gmail.com>
2023-06-15 20:09:56 +0300

    added goalRoute.ts

---
+++ server/routes/goalRoute.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import categoryCtrl from "../controllers/categoryCtrl";
+import auth from "../middleware/auth";
+
+const router = express.Router();
+
+// @route  GET, POST api/category
+// @desc   Get, Create category
+// @access Public, Private
+router
+  .route("/category")
+  .get(categoryCtrl.getCategory)
+  .post(auth, categoryCtrl.createCategory);
+
+// @route  PATCH, DELETE api/category/:id
+// @desc   Update, Delete category
+// @access Private
+router
+  .route("/category/:id")
+  .patch(auth, categoryCtrl.updateCategory)
+  .delete(auth, categoryCtrl.deleteCategory);
+
+export default router;
