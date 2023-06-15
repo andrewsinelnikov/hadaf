224c05feadaa7074206d04fb9145f89275ced031
Andrew <ansinelnikov@gmail.com>
2023-06-15 20:08:48 +0300

    added createGoal()

--- server/controllers/goalCtrl.ts
+++ server/controllers/goalCtrl.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User from "../models/userModel";
+import Goal from "../models/goalModel";
 import { IReqAuth } from "../config/interface";
 import bcrypt from "bcrypt";
 
@@ -9,62 +9,15 @@ const goalCtrl = {
       return res.status(400).json({ msg: "Invalid Authentication" });
 
     try {
-      const { title, author, source } = req.body;
+      const { text } = req.body;
 
-      // const newPublication = new Publication({
-      //   user: req.user._id,
-      //   type: "book",
-      //   title,
-      //   title: title.toLowerCase(),
-      //   author,
-      //   source,
-      // });
-      // await newPublication.save();
+      const newGoal = new Goal({ user: req.user._id, text });
+      await newGoal.save();
 
-      // res.json({
-      //   ...newPublication._doc,
-      //   user: req.user,
-      // });
-    } catch (err: any) {
-      return res.status(500).json({ msg: err.message });
-    }
-  },
-  updateUser: async (req: IReqAuth, res: Response) => {
-    if (!req.user)
-      return res.status(400).json({ msg: "Invalid Authentication" });
-
-    try {
-      const { image, name, usta, bbook } = req.body;
-
-      await User.findOneAndUpdate(
-        { _id: req.user._id },
-        { image, name, usta, bbook }
-      );
-
-      res.json({ msg: "Update Success" });
-    } catch (err: any) {
-      return res.status(500).json({ msg: err.message });
-    }
-  },
-  resetPassword: async (req: IReqAuth, res: Response) => {
-    if (!req.user)
-      return res.status(400).json({ msg: "Invalid Authentication" });
-
-    if (req.user.type !== "register")
-      return res.status(400).json({
-        msg: `Quick login account with ${req.user.type} can't use this function`,
+      res.json({
+        ...newGoal._doc,
+        user: req.user,
       });
-
-    try {
-      const { password } = req.body;
-      const passwordHash = await bcrypt.hash(password, 12);
-
-      await User.findOneAndUpdate(
-        { _id: req.user._id },
-        { password: passwordHash }
-      );
-
-      res.json({ msg: "Reset Password Success!" });
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
