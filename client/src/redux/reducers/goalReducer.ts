e9a64dd8e946cfed87d3dd9483a6840c57c1a867
Andrew <ansinelnikov@gmail.com>
2023-06-16 09:00:40 +0300

    created goalReducer.ts

---
+++ client/src/redux/reducers/goalReducer.ts
@@ -0,0 +1,32 @@
+import {
+  CREATE_CATEGORY,
+  DELETE_CATEGORY,
+  GET_CATEGORIES,
+  ICategoryType,
+  UPDATE_CATEGORY,
+} from "../types/categoryType";
+import { ICategory } from "../../utils/TypeScript";
+
+const categoryReducer = (
+  state: ICategory[] = [],
+  action: ICategoryType
+): ICategory[] => {
+  switch (action.type) {
+    case CREATE_CATEGORY:
+      return [action.payload, ...state];
+    case GET_CATEGORIES:
+      return action.payload;
+    case UPDATE_CATEGORY:
+      return state.map((item) =>
+        item._id === action.payload._id
+          ? { ...item, name: action.payload.name }
+          : item
+      );
+    case DELETE_CATEGORY:
+      return state.filter((item) => item._id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default categoryReducer;
