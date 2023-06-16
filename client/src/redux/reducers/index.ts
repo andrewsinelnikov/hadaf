5f05a11c7106fb0b6fe10ca789c47bb7b8718c46
Andrew <ansinelnikov@gmail.com>
2023-06-16 09:06:01 +0300

    added goals reducer

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import auth from "./authReducer";
 import alert from "./alertReducer";
 import categories from "./categoryReducer";
+import goals from "./goalReducer";
 
 export default combineReducers({
   auth,
   alert,
   categories,
+  goals,
 });
