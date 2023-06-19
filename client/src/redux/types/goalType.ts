186d1b06c7809bde4d0e3695dd5f97b00f38a266
Andrew <ansinelnikov@gmail.com>
2023-06-19 20:30:29 +0300

    added IUpdateGoal interface

--- client/src/redux/types/goalType.ts
+++ client/src/redux/types/goalType.ts
@@ -2,8 +2,7 @@ import { IItem } from "../../utils/TypeScript";
 
 export const CREATE_GOAL = "CREATE_GOAL";
 export const GET_GOALS = "GET_GOALS";
-// export const GET_CATEGORIES = "GET_CATEGORIES";
-// export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const UPDATE_GOAL = "UPDATE_GOAL";
 // export const DELETE_CATEGORY = "DELETE_CATEGORY";
 
 export interface ICreateGoal {
@@ -16,21 +15,16 @@ export interface IGetGoals {
   payload: IItem[];
 }
 
-// export interface IGetCategories {
-//   type: typeof GET_CATEGORIES;
-//   payload: ICategory[];
-// }
-
-// export interface IUpdateCategory {
-//   type: typeof UPDATE_CATEGORY;
-//   payload: ICategory;
-// }
+export interface IUpdateGoal {
+  type: typeof UPDATE_GOAL;
+  payload: IItem;
+}
 
 // export interface IDeleteCategory {
 //   type: typeof DELETE_CATEGORY;
 //   payload: string;
 // }
 
-export type IGoalType = ICreateGoal| IGetGoals;
+export type IGoalType = ICreateGoal | IGetGoals;
 // | IUpdateCategory
 // | IDeleteCategory;
