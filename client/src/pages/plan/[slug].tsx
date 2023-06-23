eb6d4da434c1ab522b05cbeef6ae5e6b22eab123
Andrew <ansinelnikov@gmail.com>
2023-06-23 10:42:00 +0300

    added PlanForGoal component

--- client/src/pages/plan/[slug].tsx
+++ client/src/pages/plan/[slug].tsx
@@ -1,38 +1,54 @@
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { RootState } from "../../redux/store";
 import { useAppSelector } from "../../utils/hooks";
-import { IParams } from "../../utils/TypeScript";
 import UserLayout from "../../components/layouts/UserLayout";
-import Layout from "../../components/layouts/Layout";
 import UserInfo from "../../components/profile/UserInfo";
-import UserEdit from "../../components/profile/UserEdit";
-import OtherInfo from "../../components/profile/OtherInfo";
-import UserPosts from "../../components/profile/UserPosts";
+import TimeReminder from "../../components/workboard/TimeReminder";
+import { IItem } from "../../utils/TypeScript";
+import Footer from "../../components/global/Footer";
 
-const Profile = () => {
-  const { slug, action }: IParams = useParams<keyof IParams>() as IParams;
+const PlanForGoal = () => {
   const { auth } = useAppSelector((state: RootState) => state);
+  const navigate = useNavigate();
 
-  if (auth.user?._id === slug && action === "edit")
-    return (
-      <Layout navbarType={1}>
-        <div className='profile-edit'>
-          <UserEdit />
-        </div>
-      </Layout>
-    );
+  useEffect(() => {
+    if (!auth.access_token) navigate("/login");
+  }, [auth.access_token, navigate]);
+
+  const [planItem, setPlanItem] = useState<string>("");
+  const [plan, setPlan] = useState<Array<IItem>>([]);
+
+  const addPlanItem = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // if (planItem) {
+    //   setPlan([...plan, { _id: Date.now(), text: planItem, isDone: false }]);
+    //   setPlanItem("");
+    // }
+  };
 
   return (
     <UserLayout navbarType={1}>
       <div className='profile'>
-        {auth.user?._id === slug ? <UserInfo /> : <OtherInfo />}
+        <UserInfo />
         <div className='profile-content'>
-          <UserPosts />
+          <div className='content'>
+            <TimeReminder action='plans' type='season' />
+          </div>
+          <Footer />
         </div>
+        {/* <ItemInput
+          item={planItem}
+          setItem={setPlanItem}
+          itemType='Plan Item'
+          items={plan}
+          handleAdd={addPlanItem}
+        /> */}
       </div>
     </UserLayout>
   );
 };
 
-export default Profile;
+export default PlanForGoal;
