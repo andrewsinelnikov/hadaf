2f5650b672c0ad9a3401b69e7693e5b617dc3242
Andrew <ansinelnikov@gmail.com>
2023-06-28 10:58:48 +0300

    added validateItem

--- client/src/pages/plan/[slug].tsx
+++ client/src/pages/plan/[slug].tsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 
 import { RootState } from "../../redux/store";
 import { IParams } from "../../utils/TypeScript";
-import { useAppSelector } from "../../utils/hooks";
+import { useAppDispatch, useAppSelector } from "../../utils/hooks";
 
 import UserLayout from "../../components/layouts/UserLayout";
 import UserInfo from "../../components/profile/UserInfo";
@@ -12,10 +12,14 @@ import { IItem } from "../../utils/TypeScript";
 import ItemList from "../../components/workboard/ItemList";
 import Footer from "../../components/global/Footer";
 import ItemInput from "../../components/workboard/ItemInput";
+import { validateItem } from "../../utils/Validate";
+import { ALERT } from "../../redux/types/alertType";
 
 const PlanForGoal = () => {
   const { slug }: IParams = useParams<keyof IParams>() as IParams;
   const { auth, goals } = useAppSelector((state: RootState) => state);
+  const dispatch = useAppDispatch();
+
   const navigate = useNavigate();
 
   const [activeGoal, setActiveGoal] = useState<IItem>();
@@ -40,6 +44,10 @@ const PlanForGoal = () => {
 
   const addPlanItem = (e: React.FormEvent) => {
     e.preventDefault();
+
+    const check = validateItem(planItem, "Please type a step toward the goal");
+    if (check.errLength !== 0)
+      return dispatch({ type: ALERT, payload: { errors: check.errMsg } });
 
     if (planItem) {
       setPlan([...plan, planItem]);
