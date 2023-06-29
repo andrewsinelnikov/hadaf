237df1db4073065966628c5cafc471f81d623d95
Andrew <ansinelnikov@gmail.com>
2023-06-29 23:07:48 +0300

    added btn

--- client/src/components/workboard/Item.tsx
+++ client/src/components/workboard/Item.tsx
@@ -104,37 +104,56 @@ const Item: React.FC<IProps> = ({ item, type }) => {
       {type === "plans" && (
         <form>
           {edit ? (
-            <div className='add-item'>
-              <input
-                className='item-input'
-                type='text'
-                name='text'
-                value={text}
-                onChange={(e) => {
-                  setText(e.target.value);
-                }}
-                autoComplete='off'
-                maxLength={200}
-                style={{
-                  borderBottom: "1px solid var(--lightdark-color)",
-                }}
-              />
-              {text.length > 0 && (
-                <small style={{ fontWeight: "bold" }}>
-                  <span
-                    style={{
-                      color: `${
-                        text.length > 200
-                          ? "var(--error-color)"
-                          : "var(--done-color)"
-                      }`,
-                    }}>
-                    {text.length}
-                  </span>{" "}
-                  / 200
-                </small>
-              )}
-            </div>
+            <>
+              <div className='add-item'>
+                <input
+                  className='item-input'
+                  type='text'
+                  name='text'
+                  value={text}
+                  onChange={(e) => {
+                    setText(e.target.value);
+                  }}
+                  autoComplete='off'
+                  maxLength={200}
+                  style={{
+                    borderBottom: "1px solid var(--lightdark-color)",
+                  }}
+                />
+                {text.length > 0 && (
+                  <small style={{ fontWeight: "bold" }}>
+                    <span
+                      style={{
+                        color: `${
+                          text.length > 200
+                            ? "var(--error-color)"
+                            : "var(--done-color)"
+                        }`,
+                      }}>
+                      {text.length}
+                    </span>{" "}
+                    / 200
+                  </small>
+                )}
+              </div>
+              <div className='item-options'>
+                <button
+                  type='button'
+                  // onClick={edit ? () => handleEdit() : () => setEdit(item)}
+                  className='btn btn-auto btn-action btn-dark'>
+                  Update
+                </button>
+                <button
+                  type='button'
+                  // onClick={() => handleDelete(item._id!)}
+                  className='btn btn-auto btn-action btn-dark'>
+                  Delete
+                </button>
+                {/* <Link to={`/plan/${item._id}`} className='btn btn-auto btn-success'>
+              Reach it
+            </Link> */}
+              </div>
+            </>
           ) : (
             <div className='item-plan'>
               <div className='text'>{item.text}</div>
