e337a4a69731a24d0bf25b978f68f69e5ea0f775
Andrew <ansinelnikov@gmail.com>
2023-06-29 22:27:40 +0300

    added edit

--- client/src/components/workboard/Item.tsx
+++ client/src/components/workboard/Item.tsx
@@ -136,8 +136,15 @@ const Item: React.FC<IProps> = ({ item, type }) => {
               )}
             </div>
           ) : (
-            // <div className='item-value'>{item.text}</div>
-            <div className='item-plan'>{item.text}</div>
+            <>
+              <div className='item-plan'>{item.text}</div>
+              <button
+                type='button'
+                onClick={() => setEdit(item)}
+                className='btn btn-auto btn-action btn-dark'>
+                Edit
+              </button>
+            </>
           )}
         </form>
       )}
