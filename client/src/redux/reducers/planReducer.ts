7a2a9c33bb545f8588e9f9aded58a7a7bbf46881
Andrew <ansinelnikov@gmail.com>
2023-07-01 17:41:31 +0300

    created plan reducer

---
+++ client/src/redux/reducers/planReducer.ts
@@ -0,0 +1,31 @@
+import {
+  IGoalType,
+  CREATE_GOAL,
+  GET_GOALS,
+  GET_CURRENT_GOALS,
+  UPDATE_GOAL,
+  DELETE_GOAL,
+} from "../types/goalType";
+import { IItem } from "../../utils/TypeScript";
+
+const goalReducer = (state: IItem[] = [], action: IGoalType): IItem[] => {
+  switch (action.type) {
+    case CREATE_GOAL:
+      return [action.payload, ...state];
+    case GET_GOALS:
+    case GET_CURRENT_GOALS:
+      return action.payload;
+    case UPDATE_GOAL:
+      return state.map((item) =>
+        item._id === action.payload._id
+          ? { ...item, text: action.payload.text }
+          : item
+      );
+    case DELETE_GOAL:
+      return state.filter((item) => item._id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default goalReducer;
