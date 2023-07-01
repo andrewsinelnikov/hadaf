e328713d231f8771f30736603ec0aac377ab6121
Andrew <ansinelnikov@gmail.com>
2023-07-01 17:50:52 +0300

    added plan reducer

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -3,10 +3,12 @@ import auth from "./authReducer";
 import alert from "./alertReducer";
 import categories from "./categoryReducer";
 import goals from "./goalReducer";
+import plans from "./planReducer";
 
 export default combineReducers({
   auth,
   alert,
   categories,
   goals,
+  plans,
 });
