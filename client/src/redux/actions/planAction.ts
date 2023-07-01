d2614c57a25836112fe8f97ba751542425a1e169
Andrew <ansinelnikov@gmail.com>
2023-07-01 15:56:43 +0300

    added createPlanItem()

--- client/src/redux/actions/planAction.ts
+++ client/src/redux/actions/planAction.ts
@@ -1,29 +1,22 @@
 import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertType";
-import {
-  CREATE_GOAL,
-  GET_CURRENT_GOALS,
-  GET_GOALS,
-  UPDATE_GOAL,
-  DELETE_GOAL,
-  IGoalType,
-} from "../types/goalType";
+import { CREATE_PLAN_ITEM, IPlanType } from "../types/planType";
 import { postAPI, getAPI, patchAPI, deleteAPI } from "../../utils/FetchData";
 import { IItem } from "../../utils/TypeScript";
 
 import { checkTokenExp } from "../../utils/checkTokenExp";
 
 export const createPlanItem =
-  (goal: IItem, token: string) =>
-  async (dispatch: Dispatch<IAlertType | IGoalType>) => {
+  (planItem: IItem, token: string) =>
+  async (dispatch: Dispatch<IAlertType | IPlanType>) => {
     const result = await checkTokenExp(token, dispatch);
     const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
 
-      const res = await postAPI("goal", goal, access_token);
+      const res = await postAPI("plan", planItem, access_token);
 
-      dispatch({ type: CREATE_GOAL, payload: res.data.newGoal });
+      dispatch({ type: CREATE_PLAN_ITEM, payload: res.data.newPlanItem });
 
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
@@ -31,62 +24,62 @@ export const createPlanItem =
     }
   };
 
-export const getGoals =
-  (token: string) => async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
-    try {
-      dispatch({ type: ALERT, payload: { loading: true } });
+// export const getGoals =
+//   (token: string) => async (dispatch: Dispatch<IAlertType | IGoalType>) => {
+//     const result = await checkTokenExp(token, dispatch);
+//     const access_token = result ? result : token;
+//     try {
+//       dispatch({ type: ALERT, payload: { loading: true } });
 
-      const res = await getAPI("goals", access_token);
-      dispatch({ type: GET_GOALS, payload: res.data.goals });
+//       const res = await getAPI("goals", access_token);
+//       dispatch({ type: GET_GOALS, payload: res.data.goals });
 
-      dispatch({ type: ALERT, payload: { loading: false } });
-    } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-    }
-  };
+//       dispatch({ type: ALERT, payload: { loading: false } });
+//     } catch (err: any) {
+//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+//     }
+//   };
 
-export const getCurrentGoals =
-  (token: string) => async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
-    try {
-      dispatch({ type: ALERT, payload: { loading: true } });
+// export const getCurrentGoals =
+//   (token: string) => async (dispatch: Dispatch<IAlertType | IGoalType>) => {
+//     const result = await checkTokenExp(token, dispatch);
+//     const access_token = result ? result : token;
+//     try {
+//       dispatch({ type: ALERT, payload: { loading: true } });
 
-      const res = await getAPI("current", access_token);
-      dispatch({ type: GET_CURRENT_GOALS, payload: res.data.goals });
+//       const res = await getAPI("current", access_token);
+//       dispatch({ type: GET_CURRENT_GOALS, payload: res.data.goals });
 
-      dispatch({ type: ALERT, payload: { loading: false } });
-    } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-    }
-  };
+//       dispatch({ type: ALERT, payload: { loading: false } });
+//     } catch (err: any) {
+//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+//     }
+//   };
 
-export const updateGoal =
-  (data: IItem, token: string) =>
-  async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
-    try {
-      dispatch({ type: UPDATE_GOAL, payload: data });
+// export const updateGoal =
+//   (data: IItem, token: string) =>
+//   async (dispatch: Dispatch<IAlertType | IGoalType>) => {
+//     const result = await checkTokenExp(token, dispatch);
+//     const access_token = result ? result : token;
+//     try {
+//       dispatch({ type: UPDATE_GOAL, payload: data });
 
-      await patchAPI(`goal/${data._id}`, { text: data.text }, access_token);
-    } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-    }
-  };
+//       await patchAPI(`goal/${data._id}`, { text: data.text }, access_token);
+//     } catch (err: any) {
+//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+//     }
+//   };
 
-export const deleteGoal =
-  (id: string, token: string) =>
-  async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
-    try {
-      dispatch({ type: DELETE_GOAL, payload: id });
+// export const deleteGoal =
+//   (id: string, token: string) =>
+//   async (dispatch: Dispatch<IAlertType | IGoalType>) => {
+//     const result = await checkTokenExp(token, dispatch);
+//     const access_token = result ? result : token;
+//     try {
+//       dispatch({ type: DELETE_GOAL, payload: id });
 
-      await deleteAPI(`goal/${id}`, access_token);
-    } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-    }
-  };
+//       await deleteAPI(`goal/${id}`, access_token);
+//     } catch (err: any) {
+//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+//     }
+//   };
--- client/src/redux/types/planType.ts
+++ client/src/redux/types/planType.ts
@@ -1,34 +1,34 @@
 import { IItem } from "../../utils/TypeScript";
 
 export const CREATE_PLAN_ITEM = "CREATE_PLAN_ITEM";
-export const GET_GOALS = "GET_GOALS";
-export const GET_CURRENT_GOALS = "GET_CURRENT_GOALS";
-export const UPDATE_GOAL = "UPDATE_GOAL";
-export const DELETE_GOAL = "DELETE_GOAL";
+// export const GET_GOALS = "GET_GOALS";
+// export const GET_CURRENT_GOALS = "GET_CURRENT_GOALS";
+// export const UPDATE_GOAL = "UPDATE_GOAL";
+// export const DELETE_GOAL = "DELETE_GOAL";
 
 export interface ICreatePlanItem {
   type: typeof CREATE_PLAN_ITEM;
   payload: IItem;
 }
 
-export interface IGetGoals {
-  type: typeof GET_GOALS;
-  payload: IItem[];
-}
+// export interface IGetGoals {
+//   type: typeof GET_GOALS;
+//   payload: IItem[];
+// }
 
-export interface IGetCurrentGoals {
-  type: typeof GET_CURRENT_GOALS;
-  payload: IItem[];
-}
+// export interface IGetCurrentGoals {
+//   type: typeof GET_CURRENT_GOALS;
+//   payload: IItem[];
+// }
 
-export interface IUpdateGoal {
-  type: typeof UPDATE_GOAL;
-  payload: IItem;
-}
+// export interface IUpdateGoal {
+//   type: typeof UPDATE_GOAL;
+//   payload: IItem;
+// }
 
-export interface IDeleteGoal {
-  type: typeof DELETE_GOAL;
-  payload: string;
-}
+// export interface IDeleteGoal {
+//   type: typeof DELETE_GOAL;
+//   payload: string;
+// }
 
 export type IPlanType = ICreatePlanItem;
