0da9399de7acde6ca421ea8f88c368447974bf01
Andrew <ansinelnikov@gmail.com>
2023-07-03 22:19:11 +0300

    added GET_PLANS_BY_GOAL case

--- client/src/redux/reducers/plansGoalReducer.ts
+++ client/src/redux/reducers/plansGoalReducer.ts
@@ -1,14 +1,24 @@
 import {
-  IPlanType,
-  CREATE_PLAN_ITEM,
+  IPlansGoal,
+  IGetPlansByGoal,
+  GET_PLANS_BY_GOAL,
   // GET_PLANS_BY_GOAL,
 } from "../types/planType";
 import { IItem } from "../../utils/TypeScript";
 
-const plansGoalReducer = (state: IItem[] = [], action: IPlanType): IItem[] => {
+const plansGoalReducer = (
+  state: IPlansGoal[] = [],
+  action: IGetPlansByGoal
+): IPlansGoal[] => {
   switch (action.type) {
-    case CREATE_PLAN_ITEM:
-      return [action.payload, ...state];
+    case GET_PLANS_BY_GOAL:
+      if (state.every((item) => item.goal !== action.payload.goal)) {
+        return [...state, action.payload];
+      } else {
+        return state.map((plan) =>
+          plan.goal === action.payload.goal ? action.payload : plan
+        );
+      }
     // case GET_PLANS_BY_GOAL:
     //   return action.payload;
     // case UPDATE_GOAL:
