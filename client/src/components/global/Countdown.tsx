270f3c32afd525766926104cac187a1e8cc5f1f9
Andrew <ansinelnikov@gmail.com>
2023-07-04 16:50:30 +0300

    changed leftTime

--- client/src/components/global/Countdown.tsx
+++ client/src/components/global/Countdown.tsx
@@ -21,14 +21,14 @@ const Countdown = ({ date, setDays }: IProps) => {
   }, [date]);
 
   const leftTime = () => {
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
     if (countdownTime) {
       const currentTime = new Date().getTime();
       const gap = countdownTime - currentTime;
-
-      let second = 1000;
-      let minute = second * 60;
-      let hour = minute * 60;
-      let day = hour * 24;
 
       let days = Math.floor(gap / day);
       let hours = Math.floor((gap % day) / hour);
