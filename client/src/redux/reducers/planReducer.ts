d44593418b4a1b9579ff18e6171bde711a419a54
Andrew <ansinelnikov@gmail.com>
2023-07-05 12:19:06 +0300

    added UPDATE_PLAN_ITEM case of reducer

--- client/src/redux/reducers/planReducer.ts
+++ client/src/redux/reducers/planReducer.ts
@@ -1,6 +1,7 @@
 import {
   IPlanType,
   CREATE_PLAN_ITEM,
+  UPDATE_PLAN_ITEM,
   // GET_PLANS_BY_GOAL,
 } from "../types/planType";
 import { IItem } from "../../utils/TypeScript";
@@ -9,14 +10,12 @@ const planReducer = (state: IItem[] = [], action: IPlanType): IItem[] => {
   switch (action.type) {
     case CREATE_PLAN_ITEM:
       return [action.payload, ...state];
-    // case GET_PLANS_BY_GOAL:
-    //   return action.payload;
-    // case UPDATE_GOAL:
-    //   return state.map((item) =>
-    //     item._id === action.payload._id
-    //       ? { ...item, text: action.payload.text }
-    //       : item
-    //   );
+    case UPDATE_PLAN_ITEM:
+      return state.map((item) =>
+        item._id === action.payload._id
+          ? { ...item, text: action.payload.text }
+          : item
+      );
     // case DELETE_GOAL:
     //   return state.filter((item) => item._id !== action.payload);
     default:
