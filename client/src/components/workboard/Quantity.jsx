992fc81755340d7062a25823926130a41cce8b47
Andrew <ansinelnikov@gmail.com>
2023-07-05 11:35:29 +0300

    created Quantity component

--- client/src/components/workboard/Quantity.jsx
+++ client/src/components/workboard/Quantity.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Quantity = () => {
-  return <div>Quantity</div>;
+  return (
+    <div className='quantity'>
+      <select onChange={selectChange}>
+        <option disabled selected>
+          How often
+        </option>
+        <option value='Daily'>Daily</option>
+        <option value='Weekly'>Weekly</option>
+        <option value='Monthly'>Monthly</option>
+        <option value='Seasonly'>Seasonly</option>
+      </select>
+      {selectPeriod &&
+        (selectPeriod === "Weekly"
+          ? timesOptions(6)
+          : selectPeriod === "Monthly"
+          ? timesOptions(4)
+          : selectPeriod === "Seasonly"
+          ? timesOptions(5)
+          : "")}
+    </div>
+  );
 };
 
 export default Quantity;
