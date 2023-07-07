38d59099518d5c70607633165896247fe7c1525d
Andrew <ansinelnikov@gmail.com>
2023-07-07 10:48:30 +0300

    added IProps interface

--- client/src/components/global/Tabs/Tab.tsx
+++ client/src/components/global/Tabs/Tab.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+interface IProps {
+  id: string;
+  title: string;
+  selectedTab: number;
+  index: number;
+  tabPanelId: string;
+  handleChanged: (event: any) => void;
+  tabRef: React.LegacyRef<HTMLButtonElement>;
+}
+
 const Tab = () => {
   return (
     <div>
