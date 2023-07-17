486c2d6c5e7fb3dafb29e627216272bc55601ef8
Andrew <ansinelnikov@gmail.com>
2023-07-17 19:57:00 +0300

    changed structure

--- client/src/components/workboard/Item.tsx
+++ client/src/components/workboard/Item.tsx
@@ -51,7 +51,7 @@ const Item: React.FC<IProps> = ({ item, type }) => {
     if (type === "goals") dispatch(deleteGoal(id, auth.access_token));
     if (type === "plans") {
       dispatch(deletePlanItem(id, auth.access_token));
-      dispatch(updateGoal(planItem, auth.access_token, true));
+      dispatch(updateGoal(item, auth.access_token, true));
       dispatch(getPlansByGoal(edit?.goal!, auth.access_token));
     }
   };
