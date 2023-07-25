0a3c15ca2d620342b97f533b7776ba7557b346c6
Andrew <ansinelnikov@gmail.com>
2023-07-25 19:40:02 +0300

    added GET_CURRENT_GOALS case

--- client/src/redux/reducers/planReducer.ts
+++ client/src/redux/reducers/planReducer.ts
@@ -1,6 +1,7 @@
 import {
   IPlanType,
   CREATE_PLAN_ITEM,
+  GET_CURRENT_PLANS,
   UPDATE_PLAN_ITEM,
   DELETE_PLAN_ITEM,
 } from "../types/planType";
@@ -10,6 +11,8 @@ const planReducer = (state: IItem[] = [], action: IPlanType): IItem[] => {
   switch (action.type) {
     case CREATE_PLAN_ITEM:
       return [action.payload, ...state];
+    case GET_CURRENT_PLANS:
+      return action.payload;
     case UPDATE_PLAN_ITEM:
       return state.map((item) =>
         item._id === action.payload._id
