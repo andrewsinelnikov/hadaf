723bfc742ba6f07ea25985860c3124d52f9f14dd
Andrew <ansinelnikov@gmail.com>
2023-07-25 19:08:22 +0300

    added getCurrentPlans ctrl

--- server/controllers/planCtrl.ts
+++ server/controllers/planCtrl.ts
@@ -3,6 +3,7 @@ import PlanItem from "../models/planModel";
 import Goal from "../models/goalModel";
 import { IReqAuth } from "../config/interface";
 import bcrypt from "bcrypt";
+import { getSeason } from "../config/getSeason";
 
 const planCtrl = {
   createPlanItem: async (req: IReqAuth, res: Response) => {
@@ -33,7 +34,43 @@ const planCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
-  getCurrentPlans: async (req: IReqAuth, res: Response) => {},
+  getCurrentPlans: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ msg: "Invalid Authentication" });
+
+    try {
+      const date = new Date();
+      const season = getSeason(date);
+
+      const startDates = {
+        spring: new Date(`${date.getFullYear()}-03-01`),
+        summer: new Date(`${date.getFullYear()}-06-01`),
+        autumn: new Date(`${date.getFullYear()}-09-01`),
+        winter: new Date(`${date.getFullYear()}-12-01`),
+      };
+
+      const seasonStart = startDates[season];
+
+      let nextSeasonStart;
+      if (season === "winter") {
+        nextSeasonStart = new Date(`1 Mar ${date.getFullYear() + 1}`);
+      } else {
+        nextSeasonStart = Object.values(startDates).find(
+          (date) => date > seasonStart
+        );
+      }
+
+      const plans = await PlanItem.find({
+        createdAt: {
+          $gte: seasonStart,
+          $lt: nextSeasonStart,
+        },
+      });
+      res.json({ plans });
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
   getPlansByGoal: async (req: IReqAuth, res: Response) => {
     if (!req.user)
       return res.status(400).json({ msg: "Invalid Authentication" });
