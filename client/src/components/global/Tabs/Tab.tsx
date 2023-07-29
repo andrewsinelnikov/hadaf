41e95a4d36d6a56c3ff5d4976c65ffb76b6b840d
Andrew <ansinelnikov@gmail.com>
2023-07-29 11:15:36 +0300

    changed style

--- client/src/components/global/Tabs/Tab.tsx
+++ client/src/components/global/Tabs/Tab.tsx
@@ -33,8 +33,8 @@ const Tab: React.FC<IProps> = ({
           index < today
             ? "var(--lightdark-30-color)"
             : index === today
-            ? "var(--light-color)"
-            : "var(--lightdark-50-color)"
+            ? "var(--primary-color)"
+            : "var(--lightdark-color)"
         }`,
       }}
       id={id}
