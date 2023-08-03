dbbbf4184af87fa696cd8bbb861144e39ebe7f6c
Andrew <ansinelnikov@gmail.com>
2023-08-03 19:58:46 +0300

    added dayModel.ts

---
+++ server/models/dayModel.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { IPlan } from "../config/interface";
+
+const planSchema = new mongoose.Schema(
+  {
+    goal: { type: mongoose.Types.ObjectId, ref: "goal" },
+    text: {
+      type: String,
+      required: [true, "Please type a plan item"],
+      trim: true,
+      maxLength: [200, "Your plan item is up to 200 chars long"],
+    },
+    period: {
+      type: String,
+      required: [true, "Please select a frequency for the item"],
+    },
+    count: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    completeness: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IPlan>("plan", planSchema);
