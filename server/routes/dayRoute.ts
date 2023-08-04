6a4b2fa2f351756c5b063dcf6c5bf5b41af1e871
Andrew <ansinelnikov@gmail.com>
2023-08-04 09:11:23 +0300

    created day route

---
+++ server/routes/dayRoute.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import planCtrl from "../controllers/planCtrl";
+import auth from "../middleware/auth";
+
+const router = express.Router();
+
+// @route  POST api/plan
+// @desc   Create a plan item
+// @access Private
+router.route("/plan").post(auth, planCtrl.createPlanItem);
+
+// @route  GET api/current_plans
+// @desc   Get current plans
+// @access Private
+router.route("/current_plans").get(auth, planCtrl.getCurrentPlans);
+
+// @route  GET api/plans/:goal
+// @desc   Get plans by goal
+// @access Private
+router.route("/plans/:goal").get(auth, planCtrl.getPlansByGoal);
+
+// @route  PATCH, DELETE api/plan/:id
+// @desc   Update, Delete plan item
+// @access Private
+router
+  .route("/plan/:id")
+  .patch(auth, planCtrl.updatePlanItem)
+  .delete(auth, planCtrl.deletePlanItem);
+
+export default router;
