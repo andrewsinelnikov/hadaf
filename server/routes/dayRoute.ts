21ea5bd2d6e7d7ff3fa4dfb2c9942dbc2a48ae93
Andrew <ansinelnikov@gmail.com>
2023-08-04 09:13:50 +0300

    added Create day's plan route

--- server/routes/dayRoute.ts
+++ server/routes/dayRoute.ts
@@ -1,30 +1,12 @@
 import express from "express";
-import planCtrl from "../controllers/planCtrl";
+import dayCtrl from "../controllers/dayCtrl";
 import auth from "../middleware/auth";
 
 const router = express.Router();
 
-// @route  POST api/plan
-// @desc   Create a plan item
+// @route  POST api/day
+// @desc   Create day's plan
 // @access Private
-router.route("/plan").post(auth, planCtrl.createPlanItem);
-
-// @route  GET api/current_plans
-// @desc   Get current plans
-// @access Private
-router.route("/current_plans").get(auth, planCtrl.getCurrentPlans);
-
-// @route  GET api/plans/:goal
-// @desc   Get plans by goal
-// @access Private
-router.route("/plans/:goal").get(auth, planCtrl.getPlansByGoal);
-
-// @route  PATCH, DELETE api/plan/:id
-// @desc   Update, Delete plan item
-// @access Private
-router
-  .route("/plan/:id")
-  .patch(auth, planCtrl.updatePlanItem)
-  .delete(auth, planCtrl.deletePlanItem);
+router.route("/day").post(auth, dayCtrl.createDay);
 
 export default router;
