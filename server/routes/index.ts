53db5b7639dfdc242c89111929dbdcc196bcad42
Andrew <ansinelnikov@gmail.com>
2023-08-04 09:14:58 +0300

    added dayRoute

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,12 +3,14 @@ import userRoute from "./userRoute";
 import categoryRoute from "./categoryRoute";
 import goalRoute from "./goalRoute";
 import planRoute from "./planRoute";
+import dayRoute from "./dayRoute";
 
 const routes = {
   authRoute,
   userRoute,
   goalRoute,
   planRoute,
+  dayRoute,
   categoryRoute,
 };
 
