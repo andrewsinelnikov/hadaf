b66d418c49528a463957a5379bb05642d301ec36
Andrew <ansinelnikov@gmail.com>
2023-08-08 19:21:55 +0300

    tested

--- server/controllers/dayCtrl.ts
+++ server/controllers/dayCtrl.ts
@@ -12,50 +12,49 @@ const dayCtrl = {
     try {
       const { date } = req.body;
       const currentDate = new Date(date);
-      console.log(currentDate);
 
-      // const newDay = new Day({ currentDate });
+      const newDay = new Day({ currentDate });
 
       // await newDay.save();
 
       //
-      // const season = getSeason(currentDate);
+      const season = getSeason(currentDate);
 
-      // const startDates = {
-      //   spring: new Date(`${currentDate.getFullYear()}-03-01`),
-      //   summer: new Date(`${currentDate.getFullYear()}-06-01`),
-      //   autumn: new Date(`${currentDate.getFullYear()}-09-01`),
-      //   winter: new Date(`${currentDate.getFullYear()}-12-01`),
-      // };
+      const startDates = {
+        spring: new Date(`${currentDate.getFullYear()}-03-01`),
+        summer: new Date(`${currentDate.getFullYear()}-06-01`),
+        autumn: new Date(`${currentDate.getFullYear()}-09-01`),
+        winter: new Date(`${currentDate.getFullYear()}-12-01`),
+      };
 
-      // const seasonStart = startDates[season];
+      const seasonStart = startDates[season];
 
-      // let nextSeasonStart;
-      // if (season === "winter") {
-      //   nextSeasonStart = new Date(`1 Mar ${currentDate.getFullYear() + 1}`);
-      // } else {
-      //   nextSeasonStart = Object.values(startDates).find(
-      //     (date) => date > seasonStart
-      //   );
-      // }
+      let nextSeasonStart;
+      if (season === "winter") {
+        nextSeasonStart = new Date(`1 Mar ${currentDate.getFullYear() + 1}`);
+      } else {
+        nextSeasonStart = Object.values(startDates).find(
+          (date) => date > seasonStart
+        );
+      }
 
-      // //
+      //
 
-      // const planItems = await PlanItem.find({
-      //   period: "Daily",
-      //   createdAt: {
-      //     $gte: seasonStart,
-      //     $lt: nextSeasonStart,
-      //   },
-      // });
+      const planItems = await PlanItem.find({
+        period: "Daily",
+        createdAt: {
+          $gte: seasonStart,
+          $lt: nextSeasonStart,
+        },
+      });
 
-      // for (const planItem of planItems) {
-      //   newDay.plans.push({ plan_id: planItem._id, done: false });
-      // }
+      for (const planItem of planItems) {
+        newDay.plans.push({ plan_id: planItem._id, done: false });
+      }
 
-      // await newDay.save();
+      await newDay.save();
 
-      // res.json({ newDay });
+      res.json({ newDay });
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
