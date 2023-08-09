5ec91ed23e11981617a6c373f475bdc71d5ed61d
Andrew <ansinelnikov@gmail.com>
2023-08-09 14:58:55 +0300

    case GET_DAY

--- client/src/redux/reducers/dayReducer.ts
+++ client/src/redux/reducers/dayReducer.ts
@@ -1,10 +1,13 @@
-import { IDayType, CREATE_DAY } from "../types/dayType";
+import { IDayType, CREATE_DAY, GET_DAY } from "../types/dayType";
 import { IDay } from "../../utils/TypeScript";
 
-const dayReducer = (state: IDay[] = [], action: IDayType): IDay[] => {
+const dayReducer = (state: IDay[] = [], action: IDayType): IDay => {
   switch (action.type) {
     case CREATE_DAY:
-      return [action.payload, ...state];
+      // return [action.payload, ...state];
+      return action.payload;
+    case GET_DAY:
+      return action.payload;
     default:
       return state;
   }
