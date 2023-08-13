80bdc4fe3cb921880bea8d85a7aa070d016aec1e
Andrew <ansinelnikov@gmail.com>
2023-08-13 14:18:54 +0300

    tested

--- client/src/utils/CurrentWeek.ts
+++ client/src/utils/CurrentWeek.ts
@@ -1,9 +1,9 @@
-export const currentWeek = (today: Date) => {
+export const currentWeek = (todayDate: Date) => {
   const firstDayOfWeek = new Date(
-    today.setDate(today.getDate() - today.getDay() + 1)
+    todayDate.setDate(todayDate.getDate() - todayDate.getDay() + 1)
   );
   const lastDayOfWeek = new Date(
-    today.setDate(today.getDate() - today.getDay() + 7)
+    todayDate.setDate(todayDate.getDate() - todayDate.getDay() + 7)
   );
 
   const daysOfWeek = [];
