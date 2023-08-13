8d258b6039af6e27901447057bc4dae3431ac29d
Andrew <ansinelnikov@gmail.com>
2023-08-13 18:05:01 +0300

    updated today to begin with 1

--- client/src/utils/CurrentWeek.ts
+++ client/src/utils/CurrentWeek.ts
@@ -1,22 +1,25 @@
 export const currentWeek = (today: Date) => {
-  let firstDayOfWeek;
-  let lastDayOfWeek;
-  today.getDay() === 0
-    ? (firstDayOfWeek = new Date(today.setDate(today.getDate() - 7)))
-    : (firstDayOfWeek = new Date(
-        today.setDate(today.getDate() - today.getDay() + 1)
-      ));
-  today.getDay() === 0
-    ? (lastDayOfWeek = new Date(today.setDate(today.getDate())))
-    : (lastDayOfWeek = new Date(
-        today.setDate(today.getDate() - today.getDay() + 7)
-      ));
-  // firstDayOfWeek = new Date(
-  //   today.setDate(today.getDate() - today.getDay() + 1)
-  // );
-  // const lastDayOfWeek = new Date(
-  //   today.setDate(today.getDate() - today.getDay() + 7)
-  // );
+  // let firstDayOfWeek;
+  // let lastDayOfWeek;
+  // today.getDay() === 0
+  //   ? (firstDayOfWeek = new Date(today.setDate(today.getDate() - 7)))
+  //   : (firstDayOfWeek = new Date(
+  //       today.setDate(today.getDate() - today.getDay() + 1)
+  //     ));
+  // today.getDay() === 0
+  //   ? (lastDayOfWeek = today
+  //   : (lastDayOfWeek = new Date(
+  //       today.setDate(today.getDate() - today.getDay() + 7)
+  //     ));
+  const firstDayOfWeek = new Date(
+    today.setDate(today.getDate() - today.getDay() + 1) -
+      (today.getDay() === 0 ? 7 : 0)
+  );
+  
+  const lastDayOfWeek = new Date(
+    today.setDate(today.getDate() - today.getDay() + 7) +
+      (today.getDay() === 0 ? -7 : 0)
+  );
 
   const daysOfWeek = [];
   for (let i = firstDayOfWeek; i <= lastDayOfWeek; i.setDate(i.getDate() + 1)) {
