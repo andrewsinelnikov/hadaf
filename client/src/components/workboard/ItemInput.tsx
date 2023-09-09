cb6a0e1ba5097cfe26e1c823a496950663fb2005
Andrew <ansinelnikov@gmail.com>
2023-09-09 20:46:08 +0300

    set position

--- client/src/components/workboard/ItemInput.tsx
+++ client/src/components/workboard/ItemInput.tsx
@@ -102,7 +102,7 @@ const ItemInput: React.FC<IProps> = ({
         bottom: `${addInput === false ? "80px" : "0px"}`,
         marginLeft: `${
           (itemType === "Step" && addInput === false && "922px") ||
-          (itemType === "NoGoals" && addInput === false && "922px") ||
+          (itemType === "NoGoals" && addInput === false && "995px") ||
           (itemType === "Plan" && addInput === false && "898px") ||
           "330px"
         }`,
