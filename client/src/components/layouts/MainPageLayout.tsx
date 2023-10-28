56c9eed6babc8a5803538b988dc20e18110bd7d9
Andrew <ansinelnikov@gmail.com>
2023-10-28 20:32:52 +0300

    clear

--- client/src/components/layouts/MainPageLayout.tsx
+++ client/src/components/layouts/MainPageLayout.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Navbar from "../global/Navbar";
 
 const MainPageLayout: React.FC<{}> = ({ children }) => {
-  // https://stackoverflow.com/questions/65605219/rendre-footer-component-all-page-except-404-pages-in-react
-
   return (
     <>
       <Navbar />
