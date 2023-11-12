16f27dc72bbbd60494b8e1172494918069c9a267
Andrew <ansinelnikov@gmail.com>
2023-11-12 10:31:38 +0200

    clear

--- client/src/redux/actions/dayAction.ts
+++ client/src/redux/actions/dayAction.ts
@@ -24,22 +24,6 @@ export const createDay =
     }
   };
 
-// export const getCurrentPlans =
-//   (token: string) => async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-//     const result = await checkTokenExp(token, dispatch);
-//     const access_token = result ? result : token;
-//     try {
-//       dispatch({ type: ALERT, payload: { loading: true } });
-
-//       const res = await getAPI("current_plans", access_token);
-//       dispatch({ type: GET_CURRENT_PLANS, payload: res.data.plans });
-
-//       dispatch({ type: ALERT, payload: { loading: false } });
-//     } catch (err: any) {
-//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-//     }
-//   };
-
 export const getDay =
   (date: string, token: string) =>
   async (dispatch: Dispatch<IAlertType | IDayType>) => {
