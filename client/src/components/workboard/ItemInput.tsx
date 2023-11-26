8eae939208406005f8d3df5d521ffe0629b948ad
Andrew <ansinelnikov@gmail.com>
2023-11-26 17:29:42 +0200

    change structure

--- client/src/components/workboard/ItemInput.tsx
+++ client/src/components/workboard/ItemInput.tsx
@@ -103,6 +103,8 @@ const ItemInput: React.FC<IProps> = ({
         <select>
           <option>Option 1</option>
           <option>Option 2</option>
+          <option>Option 3</option>
+          <option>Option 4</option>
         </select>
       );
     } else {
