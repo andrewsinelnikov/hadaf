21c63b2cf770c02334f561ed457fddb285b1d4e4
Andrew <ansinelnikov@gmail.com>
2023-12-07 20:39:55 +0200

    add brand

--- server/config/sendMail.ts
+++ server/config/sendMail.ts
@@ -39,6 +39,7 @@ const sendEmail = async (to: string, url: string, txt: string) => {
       html: `
                 <div style="max-width: 700px; margin: auto; padding: 10px 20px; font-size: 110%;">
                   <div style="border: 2px solid #ab915d; padding: 10px 20px;">
+                    <h1 style="color: #ab915d; font-size: 30px; text-transform: uppercase; font-weight: 700; padding-bottom: 0.35rem;">HADAF</h1>
                     <h2 style="text-align: center; text-transform: uppercase; color: teal;">Welcome To Hadaf!</h2>
                     <p>
                         Congratulations! You're almost set to start using Hadaf.
