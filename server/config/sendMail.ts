d85f110d90d7fa15ebf7211a2d5a231b91557ccf
Andrew <ansinelnikov@gmail.com>
2023-12-07 20:28:12 +0200

    change subject

--- server/config/sendMail.ts
+++ server/config/sendMail.ts
@@ -35,7 +35,7 @@ const sendEmail = async (to: string, url: string, txt: string) => {
     const mailOptions = {
       from: SENDER_EMAIL,
       to: to,
-      subject: "HADAF",
+      subject: "HADAF - Validation your email address",
       html: `
                 <div style="max-width: 700px; margin: auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
                     <h2 style="text-align: center; text-transform: uppercase; color: teal;">Welcome To Hadaf!</h2>
