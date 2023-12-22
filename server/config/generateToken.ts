e584f3499107234dfc9e85b9548b0083b0911284
Andrew <ansinelnikov@gmail.com>
2023-12-22 19:57:11 +0200

    change generateRefreshToken

--- server/config/generateToken.ts
+++ server/config/generateToken.ts
@@ -16,17 +16,16 @@ export const generateAccessToken = (payload: object) => {
   });
 };
 
-export const generateRefreshToken = (payload: object) => {
-  // export const generateRefreshToken = (payload: object, res: Response) => {
+export const generateRefreshToken = (payload: object, res: Response) => {
   const refresh_token = jwt.sign(payload, `${REFRESH_TOKEN_SECRET}`, {
     expiresIn: "30d",
   });
 
-  // res.cookie("refreshtoken", refresh_token, {
-  //   httpOnly: true,
-  //   path: `/api/refresh_token`,
-  //   maxAge: 30 * 24 * 60 * 60 * 1000,
-  // });
+  res.cookie("refreshtoken", refresh_token, {
+    httpOnly: true,
+    path: `/api/refresh_token`,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
 
   return refresh_token;
 };
