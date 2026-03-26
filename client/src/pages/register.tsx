f991455e5692a70ce0239b6e42264c6758ce407b
Andrew Sinelnikov <ansinelnikov@gmail.com>
2026-03-26 09:32:31 +0200

    Redesign auth pages

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,28 +1,22 @@
 import { Link } from "react-router-dom";
-// import { Link, useLocation } from "react-router-dom";
 
 import Layout from "../components/layouts/Layout";
 import RegisterForm from "../components/auth/RegisterForm";
 
 const Register = () => {
-  // const location = useLocation();
-
   return (
     <Layout navbarType={0}>
       <div className='auth_page'>
         <div className='auth_box'>
-          <h3 className='auth_title'>Register</h3>
-          <RegisterForm />
+          <div className='auth_header'>
+            <h3 className='auth_title'>Start your journey</h3>
+            <p className='auth_subtitle'>
+              Already have an account?{" "}
+              <Link to='/login'>Sign in</Link>
+            </p>
+          </div>
 
-          <p style={{ marginTop: "1.5rem", textAlign: "center" }}>
-            {`Have an account? `}
-            {/* <Link to={`/login${location.search}`} style={{ color: "crimson" }}> */}
-            <Link
-              to={`/login`}
-              style={{ fontWeight: "500", textTransform: "uppercase" }}>
-              Sign in
-            </Link>
-          </p>
+          <RegisterForm />
         </div>
       </div>
     </Layout>
