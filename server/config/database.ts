d44037a819912795095f275af4acad0e70057b50
Andrew Sinelnikov <ansinelnikov@gmail.com>
2026-03-27 21:16:44 +0200

    Switched from the deprecated callback-style mongoose.connect() to the promise API.

--- server/config/database.ts
+++ server/config/database.ts
@@ -2,7 +2,21 @@ import mongoose from "mongoose";
 
 const URI = process.env.MONGODB_URL;
 
-mongoose.connect(`${URI}`, (err) => {
-  if (err) throw err;
-  console.log("MongoDB Connected...");
+if (!URI) {
+  console.error("MONGODB_URL is not set");
+  process.exit(1);
+}
+
+mongoose
+  .connect(URI, {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
+mongoose.connection.on("disconnected", () => {
+  console.warn("MongoDB disconnected");
 });
