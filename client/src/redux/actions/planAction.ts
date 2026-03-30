3f69b5a0ab1e764e048e6fa722449af3294d7aa1
Andrew Sinelnikov <ansinelnikov@gmail.com>
2026-03-30 09:14:37 +0300

    All token parameters removed from every action creator All checkTokenExp boilerplate removed

--- client/src/redux/actions/planAction.ts
+++ client/src/redux/actions/planAction.ts
@@ -1,115 +1,83 @@
 import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertType";
 import {
-  IPlanType,
-  IGetPlansByGoal,
-  CREATE_PLAN_ITEM,
-  GET_CURRENT_PLANS,
-  GET_PLANS_BY_GOAL,
-  UPDATE_PLAN_ITEM,
-  DELETE_PLAN_ITEM,
-  GET_PLAN_BY_ID,
+  IPlanType, IGetPlansByGoal,
+  CREATE_PLAN_ITEM, GET_CURRENT_PLANS, GET_PLANS_BY_GOAL,
+  UPDATE_PLAN_ITEM, DELETE_PLAN_ITEM, GET_PLAN_BY_ID,
 } from "../types/planType";
 import { postAPI, getAPI, patchAPI, deleteAPI } from "../../utils/FetchData";
-import { IItem } from "../../utils/TypeScript";
-
-import { checkTokenExp } from "../../utils/checkTokenExp";
+import { IItem } from "../../types";
 
 export const createPlanItem =
-  (planItem: IItem, token: string) =>
+  (planItem: IItem) =>
   async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await postAPI("plan", planItem, access_token);
-
+      const res = await postAPI("plan", planItem);
       dispatch({ type: CREATE_PLAN_ITEM, payload: res.data.newPlanItem });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to create plan item" } });
     }
   };
 
 export const getCurrentPlans =
-  (token: string) => async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
+  () =>
+  async (dispatch: Dispatch<IAlertType | IPlanType>) => {
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await getAPI("current_plans", access_token);
+      const res = await getAPI("current_plans");
       dispatch({ type: GET_CURRENT_PLANS, payload: res.data.plans });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to load plans" } });
     }
   };
 
 export const getPlansByGoal =
-  (goal: string, token: string) =>
+  (goal: string) =>
   async (dispatch: Dispatch<IAlertType | IGetPlansByGoal>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await getAPI(`plans/${goal}`, access_token);
+      const res = await getAPI(`plans/${goal}`);
       dispatch({ type: GET_PLANS_BY_GOAL, payload: { ...res.data, goal } });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to load plans" } });
     }
   };
 
 export const getPlanById =
-  (id: string, token: string) =>
+  (id: string) =>
   async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await getAPI(`plan/${id}`, access_token);
-
+      const res = await getAPI(`plan/${id}`);
       dispatch({ type: GET_PLAN_BY_ID, payload: res.data.plan });
-
-      // console.log(res.data.plan);
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to load plan" } });
     }
   };
 
 export const updatePlanItem =
-  (data: IItem, token: string) =>
+  (data: IItem) =>
   async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: UPDATE_PLAN_ITEM, payload: data });
-
-      await patchAPI(`plan/${data._id}`, { text: data.text }, access_token);
+      await patchAPI(`plan/${data._id}`, { text: data.text });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to update plan item" } });
     }
   };
 
 export const deletePlanItem =
-  (id: string, token: string) =>
+  (id: string) =>
   async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: DELETE_PLAN_ITEM, payload: id });
-
-      await deleteAPI(`plan/${id}`, access_token);
+      await deleteAPI(`plan/${id}`);
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to delete plan item" } });
     }
   };
