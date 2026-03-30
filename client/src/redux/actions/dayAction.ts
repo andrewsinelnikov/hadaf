a7ff22102965cbb5559fc5018fa7a68f12479649
Andrew Sinelnikov <ansinelnikov@gmail.com>
2026-03-30 09:15:08 +0300

    All token parameters removed from every action creator All checkTokenExp boilerplate removed

--- client/src/redux/actions/dayAction.ts
+++ client/src/redux/actions/dayAction.ts
@@ -1,70 +1,30 @@
 import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertType";
 import { IDayType, CREATE_DAY, GET_DAY } from "../types/dayType";
-import { postAPI, getAPI, patchAPI, deleteAPI } from "../../utils/FetchData";
-import { IDay, IItem } from "../../utils/TypeScript";
-
-import { checkTokenExp } from "../../utils/checkTokenExp";
+import { postAPI, getAPI } from "../../utils/FetchData";
 
 export const createDay =
-  (date: string, token: string) =>
+  (date: string) =>
   async (dispatch: Dispatch<IAlertType | IDayType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await postAPI("day", { date }, access_token);
-
+      const res = await postAPI("day", { date });
       dispatch({ type: CREATE_DAY, payload: res.data.newDay });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to create day" } });
     }
   };
 
 export const getDay =
-  (date: string, token: string) =>
+  (date: string) =>
   async (dispatch: Dispatch<IAlertType | IDayType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await getAPI(`day/${date}`, access_token);
+      const res = await getAPI(`day/${date}`);
       dispatch({ type: GET_DAY, payload: res.data.day });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to load day" } });
     }
   };
-
-// export const updatePlanItem =
-//   (data: IItem, token: string) =>
-//   async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-//     const result = await checkTokenExp(token, dispatch);
-//     const access_token = result ? result : token;
-//     try {
-//       dispatch({ type: UPDATE_PLAN_ITEM, payload: data });
-
-//       await patchAPI(`plan/${data._id}`, { text: data.text }, access_token);
-//     } catch (err: any) {
-//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-//     }
-//   };
-
-// export const deletePlanItem =
-//   (id: string, token: string) =>
-//   async (dispatch: Dispatch<IAlertType | IPlanType>) => {
-//     const result = await checkTokenExp(token, dispatch);
-//     const access_token = result ? result : token;
-//     try {
-//       dispatch({ type: DELETE_PLAN_ITEM, payload: id });
-
-//       await deleteAPI(`plan/${id}`, access_token);
-//     } catch (err: any) {
-//       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
-//     }
-//   };
