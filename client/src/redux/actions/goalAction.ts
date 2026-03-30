9c0500cc674390279f536ee020a545485d7931d4
Andrew Sinelnikov <ansinelnikov@gmail.com>
2026-03-30 09:13:57 +0300

    All token parameters removed from every action creator All checkTokenExp boilerplate removed

--- client/src/redux/actions/goalAction.ts
+++ client/src/redux/actions/goalAction.ts
@@ -1,100 +1,73 @@
 import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertType";
 import {
-  CREATE_GOAL,
-  GET_CURRENT_GOALS,
-  GET_GOALS,
-  UPDATE_GOAL,
-  DELETE_GOAL,
-  IGoalType,
+  CREATE_GOAL, GET_CURRENT_GOALS, GET_GOALS,
+  UPDATE_GOAL, DELETE_GOAL, IGoalType,
 } from "../types/goalType";
 import { postAPI, getAPI, patchAPI, deleteAPI } from "../../utils/FetchData";
-import { IItem } from "../../utils/TypeScript";
-
-import { checkTokenExp } from "../../utils/checkTokenExp";
+import { IItem } from "../../types";
 
 export const createGoal =
-  (goal: IItem, token: string) =>
+  (goal: IItem) =>
   async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await postAPI("goal", goal, access_token);
-
+      const res = await postAPI("goal", goal);
       dispatch({ type: CREATE_GOAL, payload: res.data.newGoal });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to create goal" } });
     }
   };
 
 export const getGoals =
-  (token: string) => async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
+  () =>
+  async (dispatch: Dispatch<IAlertType | IGoalType>) => {
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await getAPI("goals", access_token);
+      const res = await getAPI("goals");
       dispatch({ type: GET_GOALS, payload: res.data.goals });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to load goals" } });
     }
   };
 
 export const getCurrentGoals =
-  (token: string) => async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
+  () =>
+  async (dispatch: Dispatch<IAlertType | IGoalType>) => {
     try {
       dispatch({ type: ALERT, payload: { loading: true } });
-
-      const res = await getAPI("current", access_token);
+      const res = await getAPI("current");
       dispatch({ type: GET_CURRENT_GOALS, payload: res.data.goals });
-
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to load goals" } });
     }
   };
 
 export const updateGoal =
-  (data: IItem, token: string, plan?: boolean) =>
+  (data: IItem, plan?: boolean) =>
   async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: UPDATE_GOAL, payload: data });
-
       if (!plan) {
-        await patchAPI(`goal/${data._id}`, { text: data.text }, access_token);
+        await patchAPI(`goal/${data._id}`, { text: data.text });
       } else {
-        await patchAPI(
-          `goal/${data.goal}`,
-          { count: data.count },
-          access_token
-        );
+        await patchAPI(`goal/${data.goal}`, { count: data.count });
       }
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to update goal" } });
     }
   };
 
 export const deleteGoal =
-  (id: string, token: string) =>
+  (id: string) =>
   async (dispatch: Dispatch<IAlertType | IGoalType>) => {
-    const result = await checkTokenExp(token, dispatch);
-    const access_token = result ? result : token;
     try {
       dispatch({ type: DELETE_GOAL, payload: id });
-
-      await deleteAPI(`goal/${id}`, access_token);
+      await deleteAPI(`goal/${id}`);
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({ type: ALERT, payload: { errors: err.response?.data?.msg ?? "Failed to delete goal" } });
     }
   };
